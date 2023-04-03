7b69550b4324580a759fab4b7b56609fce004f20
Alex Godbehere <114239316+AlexGodbehere@users.noreply.github.com>
2023-04-03 08:35:29 +0100

    Fix bootstrap script (#1)

--- lib/amrc/factoryplus/krbkeys/bootstrap.py
+++ lib/amrc/factoryplus/krbkeys/bootstrap.py
@@ -15,22 +15,24 @@ import kadmin_local as kadmin
 from .kadmin import Kadm
 from .kubernetes import K8s
 
+
 class KrbKeysBs:
-    def __init__ (self, namespace, keytabs, secrets, principal, ktname):
+    def __init__(self, namespace, keytabs, secrets, principal, ktname):
         self.keytabs = keytabs
+        self.namespace = namespace
         self.secrets = secrets
         self.principal = principal
         self.ktname = ktname
-        
+
         kadm = kadmin.local()
         self.kadm = Kadm("bootstrap", kadm=kadm)
 
         k8s.config.load_incluster_config()
         self.k8s = K8s()
 
-    def ensure_keytab (self):
+    def ensure_keytab(self):
         op = self.principal
-        kt = self.k8s.read_secret(self.keytabs, self.ktname)
+        kt = self.k8s.read_secret(ns=self.namespace, name=self.keytabs, key=self.ktname)
 
         if self.kadm.princ_in_keytab(op, kt):
             logging.info(f"Keytab for {op} already exists.")
@@ -38,22 +40,23 @@ class KrbKeysBs:
 
         logging.info(f"Creating keytab for {op}")
         kt = self.kadm.create_keytab([op])
-        self.k8s.update_secret(self.keytabs, self.ktname, kt)
+        self.k8s.update_secret(ns=self.namespace, name=self.keytabs, key=self.ktname, value=kt)
 
-    def run (self):
+    def run(self):
         for s in self.secrets:
-            self.k8s.find_secret(s)
-    
+            self.k8s.find_secret(name=s, ns=self.namespace)
+
         self.ensure_keytab()
-        
+
+
 if __name__ == "__main__":
     logging.getLogger().setLevel("INFO")
 
     app = KrbKeysBs(
-        namespace = os.environ["NAMESPACE"],
-        keytabs = os.environ["KEYTABS_SECRET"],
-        secrets = [os.environ["PASSWORDS_SECRET"]],
-        principal = os.environ["OP_PRINCIPAL"],
-        ktname = os.environ["OP_KEYTAB"])
+        namespace=os.environ["NAMESPACE"],
+        keytabs=os.environ["KEYTABS_SECRET"],
+        secrets=[os.environ["PASSWORDS_SECRET"]],
+        principal=os.environ["OP_PRINCIPAL"],
+        ktname=os.environ["OP_KEYTAB"])
 
     app.run()

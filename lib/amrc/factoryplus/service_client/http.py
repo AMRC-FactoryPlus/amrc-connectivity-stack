25d049884f8325bf2369295048fc42ba148831bc
Ben Morrow <b.morrow@amrc.co.uk>
2023-06-19 12:04:14 +0100

    Fetch and use bearer tokens.

--- lib/amrc/factoryplus/service_client/http.py
+++ lib/amrc/factoryplus/service_client/http.py
@@ -3,7 +3,9 @@
 # Copyright 2023 AMRC
 
 import logging
+import urllib.parse     as urls
 
+from requests           import HTTPError
 from requests.auth      import HTTPBasicAuth
 from requests_cache     import CachedSession
 from requests_kerberos  import HTTPKerberosAuth
@@ -21,12 +23,39 @@ class HTTP (ServiceInterface):
         self.cache = "default"
 
         if "username" in fplus.opts:
+            log.info("Using HTTP Basic auth")
             self.basic = HTTPBasicAuth(
                 fplus.opts["username"], fplus.opts["password"])
         else:
+            log.info("Using HTTP Negotiate auth")
             self.basic = None
 
-    def fetch_with_creds (self, **opts):
+    def fetch_with_token (self, host, **opts):
+        headers = opts.get("headers", {})
+
+        tok = None
+        def try_req ():
+            nonlocal tok
+            tok = self.service_token(host, tok)
+            with_auth = headers | { "Authorization": f"Bearer {tok}" }
+            return self._fetch(**opts, headers=with_auth)
+
+        res = try_req()
+        if res.status_code == 401:
+            res = try_req()
+
+        return res
+
+    def service_token (self, host, bad=None):
+        tok = self.tokens.get(host)
+
+        if tok is None or tok == bad:
+            tok = self.fetch_token(host)
+            self.tokens[host] = tok
+
+        return tok
+
+    def fetch_token (self, host):
         if self.basic is not None:
             auth = self.basic
         else:
@@ -34,4 +63,18 @@ class HTTP (ServiceInterface):
             # browser requests.
             auth = HTTPKerberosAuth(force_preemptive=True)
 
-        return self.session.request(**opts, auth=auth)
+        res = self._fetch(
+            url=urls.urljoin(host, "/token"),
+            method="POST",
+            auth=auth, force_refresh=True)
+
+        if res.ok:
+            return res.json()["token"]
+
+        raise HTTPError(
+            f"Can't fetch token for {host}: {res.status_code}",
+            response=res)
+
+    def _fetch (self, **opts):
+        log.debug(f"Fetch: {opts!r}")
+        return self.session.request(**opts)

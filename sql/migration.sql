fe798375830d70bae9d5ebd17d03e4f75b11e6aa
Ben Morrow <b.morrow@amrc.co.uk>
2023-10-12 12:15:23 +0100

    SQL migration bugfix.
    I previously fixed this for the Directory. It would be good to pull this
    boilerplate from some central source somehow; maybe git submodules?

--- sql/migration.sql
+++ sql/migration.sql
@@ -18,11 +18,11 @@ create or replace procedure migrate_to (newvers integer, sql text)
             if curvers is null or curvers < newvers then
                 raise notice 'Migrating database schema to version %', newvers;
                 execute sql;
-            end if;
-            if curvers is null then
-                insert into version (version) values (newvers);
-            else
-                update version set version = newvers;
+                if curvers is null then
+                    insert into version (version) values (newvers);
+                else
+                    update version set version = newvers;
+                end if;
             end if;
         end;
     $$;

5828b3a955fa4ec2f2be727cfdffd432125d340a
Ben Morrow <b.morrow@amrc.co.uk>
2023-10-19 13:55:31 +0100

    Reconcile the principal in the Auth service.

--- lib/amrc/factoryplus/krbkeys/account.py
+++ lib/amrc/factoryplus/krbkeys/account.py
@@ -5,6 +5,8 @@
 import  typing
 from    uuid        import UUID
 
+from    amrc.factoryplus    import ServiceError
+
 from    .context    import kk_ctx
 from    .util       import fields, log
 
@@ -33,11 +35,34 @@ class FPAccount:
         log(f"Reconcile account {self}")
         fp = kk_ctx().fplus
 
+        log(f"Creating account object in class {self.klass}")
         fp.configdb.create_object(self.klass, self.uuid)
+
+        # XXX This is not atomic, but this is unavoidable with the
+        # current auth service API.
+        ids = fp.auth.get_principal(self.uuid)
+        if ids is not None and ids["kerberos"] == self.principal:
+            log(f"Principal is already correct in auth service")
+        else:
+            log(f"Updating auth principal mapping for {self.uuid} to {self.principal}")
+            if ids is not None:
+                fp.auth.delete_principal(self.uuid)
+            fp.auth.add_principal({
+                "uuid": self.uuid, 
+                "kerberos": self.principal,
+            })
 
     def remove (self, new):
         log(f"Maybe remove account: {self} -> {new}")
         if new is not None and self.uuid == new.uuid:
             log("UUIDs match, ignoring")
             return
+
+        fp = kk_ctx().fplus
+
         log(f"Removing account {self.uuid}")
+        try:
+            fp.auth.delete_principal(self.uuid)
+        except ServiceError as err:
+            if err.status != 404:
+                raise

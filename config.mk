94b6f146cc9aaf12955d4a7da5feb723afa78de5
Ben Morrow <b.morrow@amrc.co.uk>
2023-10-26 11:01:00 +0100

    Extend the database to store alerts.
    For now I am tracking and storing all alert values here. I don't quite
    like this, but working out how to pull that information from Influx is
    probably not straightforward.

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 registry=amrc-factoryplus.shef.ac.uk:5000
-deployment=directory-webapi
+deployment=directory-mqtt
 suffix=-bmz
 #acs_base=${registry}/acs-base:1.0.0
 #acs_npm=https://npm.amrc-factoryplus-dev.shef.ac.uk
---
+++ sql/v9.sql
@@ -0,0 +1,19 @@
+-- Factory+ / AMRC Connectivity Stack (ACS) Directory component
+-- Database creation/upgrade DDL.
+-- Copyright 2021 AMRC.
+
+call migrate_to(9, $migrate$
+    create table alert_type (
+        id serial primary key,
+        uuid uuid unique not null
+    );
+
+    create table alert (
+        id serial primary key,
+        uuid uuid unique not null,
+        device integer not null references device,
+        atype integer not null references alert_type,
+        metric text not null,
+        active boolean not null
+    );
+$migrate$);

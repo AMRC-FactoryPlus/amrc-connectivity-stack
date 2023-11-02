5272349e662d44b7d525c52b454d030009fb4498
Ben Morrow <b.morrow@amrc.co.uk>
2023-11-02 10:22:38 +0000

    Log version at startup.

--- bin/edge-monitor.js
+++ bin/edge-monitor.js
@@ -6,7 +6,10 @@
 
 import { ServiceClient } from "@amrc-factoryplus/utilities";
 
+import { GIT_VERSION } from "../lib/git-version.js";
 import { Monitor } from "../lib/monitor.js";
+
+console.log("Starting ACS edge monitor, revision %s", GIT_VERSION);
 
 const fplus = await new ServiceClient({
     env: process.env,

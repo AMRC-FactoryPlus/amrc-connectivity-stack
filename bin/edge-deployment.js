00c9cd6eab735ca92f469b2516c81316fdd29ba5
Ben Morrow <b.morrow@amrc.co.uk>
2023-12-06 16:07:37 +0000

    Move app startup into lib
    This is getting complicated.

--- bin/edge-deployment.js
+++ bin/edge-deployment.js
@@ -6,52 +6,10 @@
  * Copyright 2023 AMRC
  */
 
-import { ServiceClient, WebAPI } from "@amrc-factoryplus/utilities";
-
-import { GIT_VERSION } from "../lib/git-version.js";
-import { EdgeDeploy } from "../lib/edge-deploy.js";
-import { Edge } from "../lib/uuids.js";
+import { App }          from "../lib/app.js";
+import { GIT_VERSION }  from "../lib/git-version.js";
 
 console.log("Running Edge Deployment Operator revision %s", GIT_VERSION);
 
-const fplus = await new ServiceClient({
-    env:                process.env,
-    permission_group:   Edge.Perm.All,
-    git_checkouts:      process.env.GIT_CHECKOUTS_DIR,
-    git_email:          process.env.GIT_EMAIL,
-}).init();
-
-const clusters = await new Clusters({
-    fplus,
-    realm:      process.env.REALM,
-}).init();
-
-const edge = await new EdgeDeploy({
-    fplus:      fplus,
-    realm:      process.env.REALM,
-    http_url:   process.env.HTTP_API_URL,
-    cert_dir:   process.env.KUBESEAL_TEMP,
-}).init();
-
-const api = await new WebAPI({
-    http_port:  process.env.PORT,
-    realm:      process.env.REALM,
-    hostname:   process.env.HOSTNAME,
-    keytab:     process.env.SERVER_KEYTAB,
-
-    ping: {
-        service:    Edge.Service.EdgeDeployment,
-        version:    "0.0.1",
-        software:   {
-            vendor:         "AMRC",
-            application:    "acs-edge-deployment",
-            revision:       GIT_VERSION,
-        },
-    },
-    routes: app => {
-        app.use("/v1", edge.routes);
-    },
-}).init();
-
-api.run();
-clusters.run();
+const app = await new App(process.env).init();
+app.run();

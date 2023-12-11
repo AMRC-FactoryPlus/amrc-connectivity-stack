1fbadfc2797a07fd9894f06e66e5d55b4faa422e
Ben Morrow <b.morrow@amrc.co.uk>
2023-12-11 12:18:07 +0000

    We need to enable the principals
    If they already exist they might have previously been disabled.

--- lib/amrc/factoryplus/krbkeys/cluster.py
+++ lib/amrc/factoryplus/krbkeys/cluster.py
@@ -24,16 +24,21 @@ kadm = Kadm(kadm=kadm_h)
 k8s.config.load_incluster_config()
 k8o = K8s()
 
+kkusr = f"op1krbkeys/{cluster}@{realm}"
 kt = KtData(contents=None)
 with kt.kt_name() as ktname:
-    kadm.create_keytab([f"op1krbkeys/{cluster}"], ktname)
+    kadm.create_keytab([kkusr], ktname)
 k8o.update_secret(ns=namespace, name="krb-keys-keytabs",
     key="client", value=kt.contents)
+kadm.enable_princ(kkusr)
+print(f"Created krbkeys account {kkusr}")
 
-fluxusr = f"op1flux/{cluster}"
+fluxusr = f"op1flux/{cluster}@{realm}"
 fluxpw = secrets.token_urlsafe()
 kadm.set_password(fluxusr, fluxpw)
 k8o.update_secret(ns=namespace, name="flux-secrets", key="password", 
     value=fluxpw.encode())
 k8o.update_secret(ns=namespace, name="flux-secrets", key="username",
     value=fluxusr.encode())
+kadm.enable_princ(fluxusr)
+print(f"Created flux account {fluxusr}")

ae4f32c6f5e7a996288d652779525711d84e76b6
Alex Godbehere <114239316+AlexGodbehere@users.noreply.github.com>
2024-01-18 09:40:38 +0000

    Update 'ThrottleRequestsWithRedis' middleware to 'ThrottleRequests'. (#86)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ namespace App\Http;
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
             'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
             'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
             'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
             'recent' => AddPageToRecentlyVisited::class,
         ];

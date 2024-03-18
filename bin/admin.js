7ef8f04abc7ddad51160862769519f3351e7beb1
Ben Morrow <b.morrow@amrc.co.uk>
2024-03-18 10:09:19 +0000

    Start an admin interface
    Even something this minimal will be useful. The links to the services
    are hardcoded for now.

--- bin/admin.js
+++ bin/admin.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
+import express      from "express";
+import { engine }   from "express-handlebars";
+
 console.log("ACS Admin interface");
+
+const acs = {
+    domain:     process.env.EXTERNAL_DOMAIN,
+    http:       process.env.SECURE ? "https" : "http",
+};
+
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/", (req, res) => res.render("index", {
+    acs,
+    page: { title: "Admin" },
+}));
+
+app.listen(process.env.PORT);
+console.log(`Listening on port ${process.env.PORT}`);

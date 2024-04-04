029f41a2532c4be66afca41f282c99e935100830
Ben Morrow <b.morrow@amrc.co.uk>
2024-04-04 10:43:20 +0100

    Update all the Makefiles
    Now we have a monorepo, factor out the Makefiles properly. Include some
    brief documentation.
    
    Adjust a couple of Dockerfiles where they have not been updated
    correctly.

---
+++ mk/acs.git.mk
@@ -0,0 +1,31 @@
+# Makefile rules for git operations
+
+ifndef .acs.git.mk
+.acs.git.mk=1
+
+git.tag!=git describe --tags --abbrev=0
+git.sha!=git rev-parse --verify HEAD
+
+.PHONY: git.prepare git.check-committed git.pull amend
+
+git.prepare:
+	@:
+
+ifndef git.allow_dirty
+git.prepare: git.check-committed
+endif
+
+ifdef git.pull
+git.prepare: git.pull
+endif
+
+git.check-committed:
+	[ -z "$$(git status --porcelain)" ] || (git status; exit 1)
+
+git.pull:
+	git pull --ff-only
+
+amend:
+	git commit --amend -C HEAD -a
+
+endif

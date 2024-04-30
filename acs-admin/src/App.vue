a1c30e64cc6e5e291ef7847a596e976ca1a49ffd
Alex Godbehere <alex.godbehere@gmail.com>
2024-04-30 15:29:30 +0100

    Refactor UI components and change Welcome page to Home
    Modified main App.vue layout by removing a large chunk of HTML code and adjusting the appearance. Renamed and refactored 'Welcome' page into 'Home' page across the application and updated corresponding metadata. Changed the 'Live Activity' icon in the Navigation component.

--- acs-admin/src/App.vue
+++ acs-admin/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+  <div class="grid h-screen overflow-auto w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
     <div class="hidden border-r bg-muted/40 md:block">
       <div class="flex h-full max-h-screen flex-col">
         <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -31,98 +31,12 @@
         </div>
       </div>
     </div>
-    <div class="flex flex-col">
-      <header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-        <!--        <Sheet>-->
-        <!--          <SheetTrigger as-child>-->
-        <!--            <Button-->
-        <!--                variant="outline"-->
-        <!--                size="icon"-->
-        <!--                class="shrink-0 md:hidden"-->
-        <!--            >-->
-        <!--              <Menu class="h-5 w-5" />-->
-        <!--              <span class="sr-only">Toggle navigation menu</span>-->
-        <!--            </Button>-->
-        <!--          </SheetTrigger>-->
-        <!--          <SheetContent side="left" class="flex flex-col">-->
-        <!--            <nav class="grid gap-2 text-lg font-medium">-->
-        <!--              <a-->
-        <!--                  href="#"-->
-        <!--                  class="flex items-center gap-2 text-lg font-semibold"-->
-        <!--              >-->
-        <!--                <Package2 class="h-6 w-6" />-->
-        <!--                <span class="sr-only">Acme Inc</span>-->
-        <!--              </a>-->
-        <!--              <a-->
-        <!--                  href="#"-->
-        <!--                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"-->
-        <!--              >-->
-        <!--                <Home class="h-5 w-5" />-->
-        <!--                Dashboard-->
-        <!--              </a>-->
-        <!--              <a-->
-        <!--                  href="#"-->
-        <!--                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"-->
-        <!--              >-->
-        <!--                <ShoppingCart class="h-5 w-5" />-->
-        <!--                Orders-->
-        <!--                <Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">-->
-        <!--                  6-->
-        <!--                </Badge>-->
-        <!--              </a>-->
-        <!--              <a-->
-        <!--                  href="#"-->
-        <!--                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"-->
-        <!--              >-->
-        <!--                <Package class="h-5 w-5" />-->
-        <!--                Products-->
-        <!--              </a>-->
-        <!--              <a-->
-        <!--                  href="#"-->
-        <!--                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"-->
-        <!--              >-->
-        <!--                <Users class="h-5 w-5" />-->
-        <!--                Customers-->
-        <!--              </a>-->
-        <!--              <a-->
-        <!--                  href="#"-->
-        <!--                  class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"-->
-        <!--              >-->
-        <!--                <LineChart class="h-5 w-5" />-->
-        <!--                Analytics-->
-        <!--              </a>-->
-        <!--            </nav>-->
-        <!--            <div class="mt-auto">-->
-        <!--              <Card>-->
-        <!--                <CardHeader>-->
-        <!--                  <CardTitle>Upgrade to Pro</CardTitle>-->
-        <!--                  <CardDescription>-->
-        <!--                    Unlock all features and get unlimited access to our-->
-        <!--                    support team.-->
-        <!--                  </CardDescription>-->
-        <!--                </CardHeader>-->
-        <!--                <CardContent>-->
-        <!--                  <Button size="sm" class="w-full">-->
-        <!--                    Upgrade-->
-        <!--                  </Button>-->
-        <!--                </CardContent>-->
-        <!--              </Card>-->
-        <!--            </div>-->
-        <!--          </SheetContent>-->
-        <!--        </Sheet>-->
-        <div class="w-full flex-1">
-          <!--          <form>-->
-          <!--            <div class="relative">-->
-          <!--              <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />-->
-          <!--              <Input-->
-          <!--                  type="search"-->
-          <!--                  placeholder="Search products..."-->
-          <!--                  class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"-->
-          <!--              />-->
-          <!--            </div>-->
-          <!--          </form>-->
+    <div class="flex flex-col overflow-auto">
+      <header class="flex justify-between items-center border-b px-4 lg:h-[60px] flex-shrink-0 lg:px-6">
+        <div class="flex items-center justify-center gap-2">
+          <i :class="`fa-solid fa-${$route.meta.icon}`"></i>
+          <h3 class="text-lg font-bold tracking-tight">{{$route.meta.name}}</h3>
         </div>
-        <h3 class="text-lg font-bold tracking-tight">{{baseUrl}}</h3>
         <Button variant="link" size="icon" @click="s.logout">Logout</Button>
       </header>
       <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">

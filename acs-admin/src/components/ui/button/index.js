0ec15f71f28e085ce6159bccc662e53f6f6513ad
Alex Godbehere <alex.godbehere@gmail.com>
2024-05-03 11:28:07 +0100

    Align buttons better

--- acs-admin/src/components/ui/button/index.js
+++ acs-admin/src/components/ui/button/index.js
@@ -20,6 +20,7 @@ export const buttonVariants = cva(
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
       size: {
+        noPadding: "-ml-4 h-10 px-4 py-2",
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",

582c657a4011ea9111598e9a5ec9f07671c60c19
Alex Godbehere <alex.godbehere@gmail.com>
2024-05-17 16:01:18 +0100

    Update DataTable component and enhance AccessControl page
    The DataTable component has been updated to add additional functionality and improve its layout. Several changes were made to the AccessControl page, including: adding row click events, a new 'EffectivePermissions' subcomponent, and information regarding principal's effective permissions. Pagination settings were also changed and UI elements were slightly adjusted across the components.

---
+++ acs-admin/src/pages/AccessControl/EffectivePermissions.vue
@@ -0,0 +1,107 @@
+<template>
+  <SheetContent v-if="selectedRow" class="gap-6 flex flex-col overflow-auto">
+    <SheetHeader>
+      <SheetTitle>{{selectedRow.kerberos}}</SheetTitle>
+      <SheetDescription>
+        {{selectedRow.uuid}}
+      </SheetDescription>
+    </SheetHeader>
+    <div>
+      <Skeleton v-if="loading" v-for="i in 10" class="h-10 rounded-lg"/>
+      <DataTable v-else :data="permissions" :columns="columns">
+        <template #toolbar-left>
+          <div class="font-bold">Privileges</div>
+        </template>
+      </DataTable>
+    </div>
+  </SheetContent>
+</template>
+
+<script>
+import { useServiceClientStore } from '@/store/serviceClientStore.js'
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@components/ui/sheet/index.js'
+import { Skeleton } from '@components/ui/skeleton/index.js'
+import DataTable from '@components/ui/data-table/DataTable.vue'
+import { columns } from './columnsEffectivePermissions.ts'
+import { UUIDs } from '@amrc-factoryplus/service-client'
+
+export default {
+
+  name: 'EffectivePermissions',
+
+  components: {
+    DataTable,
+    Skeleton,
+    SheetHeader,
+    SheetTitle,
+    SheetContent,
+    SheetDescription,
+  },
+
+  props: {
+    selectedRow: {
+      type: Object,
+      default: null,
+    },
+  },
+
+  watch: {
+    selectedRow: {
+      handler (val) {
+        if (val == null) {
+          return
+        }
+
+        this.fetchPermissionsForPrincipal(val.kerberos)
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    async fetchPermissionsForPrincipal (principal) {
+
+      this.loading = true
+
+      const res = await this.s.client.Auth.fetch(`/authz/effective/${principal}`)
+
+      const info = o => this.s.client.ConfigDB.get_config(UUIDs.App.Info, o)
+      const name = o => info(o).then(v => v?.name)
+
+      const rv = []
+      for (const effective of res[1]) {
+        const permissionName = await name(effective.permission)
+        const targetName     = await name(effective.target)
+
+        rv.push({
+          permission: {
+            uuid: effective.permission,
+            name: permissionName,
+          },
+          target: {
+            uuid: effective.target,
+            name: targetName,
+          },
+        })
+      }
+
+      this.loading     = false
+      this.permissions = rv
+    },
+  },
+
+  setup () {
+    return {
+      columns,
+      s: useServiceClientStore(),
+    }
+  },
+
+  data () {
+    return {
+      loading: false,
+      permissions: [],
+    }
+  },
+}
+</script>
---
+++ acs-admin/src/pages/AccessControl/columnsEffectivePermissions.ts
@@ -0,0 +1,40 @@
+import type {ColumnDef} from '@tanstack/vue-table'
+import {h} from 'vue'
+import {Badge} from '@/components/ui/badge'
+
+import DataTableColumnHeader from '@/components/ui/data-table/DataTableColumnHeader.vue'
+
+interface Object {
+    uuid: string,
+    name: string,
+}
+
+export interface EffectivePermission {
+    permission: Object,
+    target: Object,
+}
+
+export const columns: ColumnDef<EffectivePermission>[] = [{
+    accessorKey: 'permission.name',
+    header: ({column}) => h(DataTableColumnHeader, {
+        column,
+        title: 'Permission'
+    }),
+
+    cell: ({row}) => {
+        return h('span', {class: 'max-w-[500px] truncate font-medium'}, row.original.permission.name)
+    },
+}, {
+    accessorKey: 'target.name',
+    header: ({column}) => h(DataTableColumnHeader, {
+        column,
+        title: 'Target'
+    }),
+
+    cell: ({row}) => {
+        if  (row.original.target.uuid === '00000000-0000-0000-0000-000000000000') {
+            return h(Badge, {variant: 'destructive'}, row.original.target.name);
+        }
+        return h('span', {class: 'max-w-[500px] truncate font-medium'}, row.original.target.name)
+    },
+}]

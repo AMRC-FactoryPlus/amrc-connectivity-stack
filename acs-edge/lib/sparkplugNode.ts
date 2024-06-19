AMRC-FactoryPlus/amrc-connectivity-stack

commit bcad293c63737f0decf93a618b0d84d746ee8b13

Forward compat for an API change
Starting to split up the `utilities` package has raised some issues with
the API. This method should always have returned a Promise; without this
it is impossible to dynamically load the Sparkplug support.

The `js-service-client` package has changed the API to return a Promise.
The `utilities` package will soon be importing its ServiceClient from
there. Fortunately JS allows you to await a non-Promise.

acs-edge/lib/sparkplugNode.ts
@@ -92,7 +92,7 @@ export class SparkplugNode extends (
             + (Math.random() * 1e17).toString(36);
 
         // conf.keepalive = 10;
-        this.#client = fplus.MQTT.basic_sparkplug_node({
+        this.#client = await fplus.MQTT.basic_sparkplug_node({
             address,
             clientId,
             publishDeath:   true,

acs-git/lib/sparkplug.js
@@ -61,7 +61,7 @@ export class SparkplugNode {
     connect_sparkplug () {
         const { ids, fplus } = this;
 
-        const cli = fplus.MQTT.basic_sparkplug_node({
+        const cli = await fplus.MQTT.basic_sparkplug_node({
             address:        ids.sparkplug,
             clientId:       ids.uuid,
             publishDeath:   true,

commit d55d99985978e69022d41547214b172c199952f6

Fix await error in the Edge Agent (#251)
Commit bcad293 introduced a bug in that it was trying to await in a
constructor. Refactor to avoid this.

acs-edge/lib/sparkplugNode.ts
@@ -73,6 +73,7 @@ class InstanceBuilder {
 export class SparkplugNode extends (
     EventEmitter
 ) {
+    #fplus: ServiceClient
     #conf: sparkplugConfig
     #client: any
     #metrics: Metrics
@@ -84,15 +85,32 @@ export class SparkplugNode extends (
 
     constructor(fplus: ServiceClient, conf: sparkplugConfig) {
         super();
+        this.#fplus = fplus;
         this.#conf = conf;
 
+        /* XXX This is about to be overwritten by .init(). But TS
+         * insists we initialise here. Making it nullable just causes
+         * mess everywhere. */
+        this.#metrics = new Metrics([]);
+        this.#metricNameIndex = {};
+        this.#metricBuffer = {}; // Buffer to hold metrics when periodic publishing enabled
+        this.#aliasCounter = 0; // Counter to keep track of metrics aliases for this Edge Node
+        /* XXX This is awful. But it's the only way to get the right
+         * type, and again we really don't want this nullable. */
+        this.#pubIntHandle = setTimeout(() => {
+        }, 1); // Handle for publish interval
+
+        this.isOnline = false; // Whether client is online or not
+    }
+
+    async init () {
          // Generate randomized client ID
-        const address = conf.address;
+        const address = this.#conf.address;
         const clientId = address.group + '-' + address.node + '-' 
             + (Math.random() * 1e17).toString(36);
 
         // conf.keepalive = 10;
-        this.#client = await fplus.MQTT.basic_sparkplug_node({
+        this.#client = await this.#fplus.MQTT.basic_sparkplug_node({
             address,
             clientId,
             publishDeath:   true,
@@ -121,7 +139,6 @@ export class SparkplugNode extends (
         this.#client.on("error", (err: Error) => this.onError(err));
         // What to do when the client connection is closed
         this.#client.on("close", () => this.onClose());
-        this.#metricNameIndex = {};
         // Default Edge Node metrics
         const builder = new InstanceBuilder(this.#conf.uuid);
         this.#metrics = new Metrics([
@@ -181,11 +198,8 @@ export class SparkplugNode extends (
                 UUIDs.Alert.ConfigInvalid, "Config_Invalid",
                 this.#conf.alerts?.configInvalid ?? false),
         ]);
-        this.isOnline = false; // Whether client is online or not
-        this.#metricBuffer = {}; // Buffer to hold metrics when periodic publishing enabled
-        this.#pubIntHandle = setTimeout(() => {
-        }, 1); // Handle for publish interval
-        this.#aliasCounter = 0; // Counter to keep track of metrics aliases for this Edge Node
+
+        return this;
     }
 
     /**

acs-edge/lib/translator.ts
@@ -86,7 +86,7 @@ export class Translator extends EventEmitter {
             };
 
             // Create sparkplug node
-            this.sparkplugNode = new SparkplugNode(this.fplus, spConf);
+            this.sparkplugNode = await new SparkplugNode(this.fplus, spConf).init();
             log(`Created Sparkplug node "${ids.sparkplug!}".`);
 
             // Create a new device connection for each type listed in config file
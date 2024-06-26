3d3ab16d191f5d98402fc93124b41a9d789bb3f6
Alex Godbehere <alex.godbehere@gmail.com>
2024-06-26 12:19:27 +0100

    Initial commit of uns-sparkplug-ingester

---
+++ uns-sparkplug-ingester/bin/ingester.ts
@@ -0,0 +1,50 @@
+/*
+ * AMRC InfluxDB Sparkplug Ingester
+ * Copyright "2023" AMRC
+ */
+
+import {ServiceClient, UUIDs} from "@amrc-factoryplus/utilities";
+import pino from "pino";
+import pretty from 'pino-pretty';
+import MQTTClient from "@lib/mqttclient.js";
+let dotenv: any = null;
+try {dotenv = await import ('dotenv')} catch (e) {}
+
+const stream = pretty({
+    colorize: true
+})
+
+dotenv?.config();
+
+const directoryUrl = process.env.DIRECTORY_URL;
+if (!directoryUrl) {
+    throw new Error("DIRECTORY_URL environment variable is not set");
+}
+
+export const logger = pino({
+    name: 'InfluxDB Sparkplug Ingester',
+    level: process.env.LOG_LEVEL || 'info',
+}, stream);
+
+
+const client = await new ServiceClient({
+    directory_url: directoryUrl,
+}).init();
+
+// Overwrite MQTT server if specified in environment
+if (process.env.MQTT_URL) {
+    client.Discovery.set_service_url(UUIDs.Service.MQTT, process.env.MQTT_URL);
+}
+
+// Overwrite Command Escalation server if specified in environment
+if (process.env.CMD_ESC_URL) {
+    client.Discovery.set_service_url(UUIDs.Service.Command_Escalation, process.env.CMD_ESC_URL);
+}
+
+const mqtt = await new MQTTClient({
+    e: {
+        serviceClient: client,
+    }
+}).init();
+
+mqtt.run();
---
+++ uns-sparkplug-ingester/run-dev.sh
@@ -0,0 +1,10 @@
+#
+# AMRC InfluxDB Sparkplug Ingester
+# Copyright "2023" AMRC
+#
+
+# bin/bash
+tmpfile=$(mktemp)
+export CLIENT_KEYTAB="$(kubectl --kubeconfig ~/.kube/p2.yaml get -n factory-plus secret krb5-keytabs -o jsonpath="{.data.sv1warehouse}" | base64 -d >"$tmpfile" && echo "$tmpfile")"
+echo $CLIENT_KEYTAB
+npm run start:shell

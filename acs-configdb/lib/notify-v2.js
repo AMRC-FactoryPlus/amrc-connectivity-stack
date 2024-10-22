ade48b85858090c7ee8e2cf0c7373c55966ed856
Ben Morrow <b.morrow@amrc.co.uk>
2024-10-22 16:18:17 +0100

    Refactor notify into a Session class

--- acs-configdb/lib/notify-v2.js
+++ acs-configdb/lib/notify-v2.js
@@ -10,6 +10,71 @@ import { v4 as uuidv4 } from "uuid";
 
 const Token_rx = /^Bearer ([A-Za-z0-9+/]+)$/;
 
+class Session {
+    constructor (opts) {
+        this.ws = opts.ws;
+
+        this.fpauth = opts.notify.api.auth;
+        this.log = (m, ...a) => 
+            opts.notify.log(`[%s] ${m}`, this.uuid, ...a);
+        this.updates = opts.notify.model.updates;
+
+        this.uuid = uuidv4();
+    }
+
+    async start () {
+        this.log("New client");
+        this.ws.addEventListener("close", () => this.log("Client closed"));
+
+        this.principal = await this.auth();
+        if (!this.principal) return;
+        this.send_updates();
+    }
+
+    async auth () {
+        const { ws } = this;
+
+        const fail = (st, msg) => {
+            this.log(msg);
+            ws.send(st);
+            ws.close();
+            return null;
+        };
+
+        const msg = await new Promise((resolve, reject) => {
+            ws.addEventListener("message", resolve, { once: true });
+            ws.addEventListener("error", reject, { once: true });
+        });
+
+        if (typeof msg.data != "string")
+            return fail("400", "Binary auth message");
+        const creds = Token_rx.exec(msg.data);
+        if (!creds)
+            return fail("400", "Bad auth message");
+
+        const princ = await this.fpauth.auth_bearer({ creds: creds[1] })
+            .catch(e => { this.log(e); return null; });
+        if (!princ)
+            return fail("401", "WS auth failed");
+
+        /* XXX authorise? */
+        ws.send("200");
+        this.log("WebSocket authenticated for %s", princ);
+        return princ;
+    }
+
+    send_updates () {
+        const { ws } = this;
+
+        const sub = this.updates.subscribe(u => {
+            const data = JSON.stringify(u, null, 2);
+            this.log("Notify %s", data);
+            ws.send(data);
+        });
+        ws.on("close", () => sub.unsubscribe());
+    }
+}
+
 export class Notify {
     constructor (opts) {
         this.log = opts.log;
@@ -25,43 +90,7 @@ export class Notify {
         this.wss.on("connection", this.new_client.bind(this));
     }
 
-    async new_client (ws) {
-        const uuid = uuidv4();
-
-        this.log("New client: %s", uuid);
-        ws.on("close", () => this.log("Client closed: %s", uuid));
-
-        const fail = (st, msg) => {
-            this.log("%s for %s", msg, uuid);
-            ws.send(st);
-            ws.close();
-        };
-
-        const msg = await new Promise((resolve, reject) => {
-            ws.addEventListener("message", resolve, { once: true });
-            ws.addEventListener("error", reject, { once: true });
-        });
-        if (typeof msg.data != "string")
-            return fail("400", "Binary auth message");
-        const creds = Token_rx.exec(msg.data);
-        if (!creds)
-            return fail("400", "Bad auth message");
-
-        const token = creds[1];
-        this.log("Verifying Bearer token [%s]", token);
-        const princ = await this.api.auth.auth_bearer({ creds: token })
-            .catch(e => { this.log(e); return null; });
-        if (!princ)
-            return fail("401", "Unknown token");
-
-        /* XXX authorise? */
-        ws.send("200");
-
-        const sub = this.model.updates.subscribe(u => {
-            const data = JSON.stringify(u, null, 2);
-            this.log("Notify %s: %s", uuid, data);
-            ws.send(data);
-        });
-        ws.on("close", () => sub.unsubscribe());
+    new_client (ws) {
+        new Session({ notify: this, ws }).start();
     }
 }

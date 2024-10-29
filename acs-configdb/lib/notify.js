6264008c381701bd1efd4fa463c3312793d313d3
Ben Morrow <b.morrow@amrc.co.uk>
2024-10-29 15:04:24 +0000

    Support WATCH app/:app/object/
    The HTTP API will accept this endpoint either with or without the
    trailing slash. For the notify interface I am going to be more strict
    and require it, as this will make relative URLs work correctly when we
    implement SEARCH.

--- acs-configdb/lib/notify.js
+++ acs-configdb/lib/notify.js
@@ -11,6 +11,82 @@ import { Notify, PathHandler }  from "@amrc-factoryplus/service-api";
 
 import { Perm } from "./constants.js";
 
+const mk_res = init => response => ({ status: init ? 201 : 200, response });
+
+function entry_response (entry) {
+    if (!entry?.config)
+        return { status: 404 };
+    const response = { status: 200, body: entry.config };
+    if (entry?.etag)
+        response.headers = { etag: entry.etag };
+    return response;
+}
+
+function list_response (list) {
+    if (!list)
+        return { status: 404 };
+    return { status: 200, body: list };
+}
+
+class CDBWatch {
+    constructor (session, app) {
+        this.session = session;
+        this.model = session.model;
+        this.app = app;
+    }
+
+    /* XXX This is not right. Until we have a push Auth API we will need
+     * to check ACLs for every update, but we should avoid sending more
+     * updates after a 403 until we get an ACL change. */
+    async check_acl (update) {
+        const { session, app } = this;
+
+        const ok = await session.check_acl(Perm.Read_App, app, true);
+        if (ok)
+            return update;
+
+        return {
+            status:     update.status,
+            response:   { status: 403 },
+        };
+    }
+
+    single_config (object) {
+        const { model, app } = this;
+
+        /* XXX Strictly there is a race condition here: the initial fetch
+         * does not slot cleanly into the sequence of updates. This would be
+         * difficult to fix. */
+        return rxx.rx(
+            rx.concat(
+                rxx.rx(
+                    model.config_get({ app, object }),
+                    rx.map(entry_response),
+                    rx.map(mk_res(true))),
+                rxx.rx(
+                    model.updates,
+                    rx.filter(u => u.app == app && u.object == object),
+                    rx.map(entry_response),
+                    rx.map(mk_res(false)))),
+            rx.flatMap(u => this.check_acl(u)));
+    }
+
+    config_list () {
+        const { model, app } = this;
+
+        /* Here we fetch the complete list every time. We could track
+         * the list contents from changes but this is safer. */
+        return rxx.rx(
+            model.updates,
+            rx.filter(u => u.app == app),
+            rx.startWith(null),
+            rx.concatMap(upd => model.config_list(app)
+                .then(list_response)
+                .then(mk_res(!!upd))),
+            rx.flatMap(u => this.check_acl(u)));
+    }
+}
+
 export class CDBNotify extends Notify {
     constructor (opts) {
         super(opts);
@@ -21,38 +97,13 @@ export class CDBNotify extends Notify {
         return [
             new PathHandler({
                 path:       "app/:app/object/:obj",
-                handler:    (...a) => this.single_config(...a),
+                handler:    (s, a, o) => new CDBWatch(s, a).single_config(o),
+            }),
+            new PathHandler({
+                path:       "app/:app/object/",
+                handler:    (s, a) => new CDBWatch(s, a).config_list(),
             }),
         ];
     }
     
-    async create_response (session, app, update) {
-        session.log("Map update: %o", update);
-        const ok = await session.check_acl(Perm.Read_App, app, true);
-        if (!ok)
-            return { status: 403 };
-        if (!update?.config)
-            return { status: 404 };
-        const res = { status: 200, body: update.config };
-        if (update.etag)
-            res.headers = { etag: update.etag };
-        return res;
-    }
-
-    single_config (session, app, object) {
-        /* XXX Strictly there is a race condition here: the initial fetch
-         * does not slot cleanly into the sequence of updates. This would be
-         * difficult to fix. */
-        return rx.concat(
-            rxx.rx(
-                session.model.config_get({ app, object }),
-                rx.flatMap(u => this.create_response(session, app, u)),
-                rx.map(response => ({ status: 201, response }))),
-            rxx.rx(
-                session.model.updates,
-                rx.filter(u => u.app == app && u.object == object),
-                rx.flatMap(u => this.create_response(session, app, u)),
-                rx.map(response => ({ status: 200, response }))),
-        );
-    }
 }

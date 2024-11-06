AMRC-FactoryPlus/js-service-client

Create a WebSocket connection to a service

lib/service/fetch.js
@@ -4,9 +4,9 @@
  * Copyright 2022 AMRC.
  */
 
-import { build_node_fetch, GSS } from "../deps.js";
+import { build_node_fetch, GSS, WebSocket } from "../deps.js";
 
-import { ServiceInterface } from "./service-interface.js";
+import { ServiceError, ServiceInterface } from "./service-interface.js";
 
 const Auth_rx = /^([A-Za-z]+) +([A-Za-z0-9._~+/=-]+)$/;
 
@@ -101,6 +101,58 @@ export default class Fetch extends ServiceInterface {
         }
     }
 
+    async websocket (opts) {
+        const base = opts.service_url
+            ?? await this.fplus.service_url(opts.service);
+        const url = new URL(opts.url, base);
+
+        const ws = new WebSocket(url);
+        await new Promise((resolve, reject) => {
+            const errh = e => {
+                const msg = e.type == "error"
+                    ? `WebSocket connection error: ${e.message}`
+                    : "WebSocket connection failed";
+                /* XXX should supply e as cause here */
+                reject(new ServiceError(opts.service, msg));
+            };
+            ws.addEventListener("open", resolve, { once: true });
+            ws.addEventListener("error", errh, { once: true });
+        });
+        return ws;
+    }
+
+    async ws_with_auth (opts) {
+        const base = await this.fplus.service_url(opts.service);
+
+        const try_ws = async bad => {
+            const ws = await this.websocket({ ...opts, service_url: base });
+            const token = await this.service_token(base, bad);
+
+            ws.send(`Bearer ${token}`);
+            const msg = await new Promise((resolve, reject) => {
+                ws.addEventListener("message", resolve, { once: true });
+                ws.addEventListener("error", reject, { once: true });
+            });
+
+            return [ws, msg.data, token];
+        };
+
+        let [ws, st, bad] = await try_ws();
+        if (st == "401")
+            [ws, st] = await try_ws(bad);
+        if (st == "200")
+            return ws;
+        
+        if (typeof st == "string" && /^[0-9]{3}$/.test(st)) {
+            throw new ServiceError(opts.service,
+                "WebSocket connection refused",
+                Number.parseInt(st, 10));
+        }
+
+        throw new ServiceError(opts.service,
+            "Unrecognised WS auth response: %s", st);
+    }
+        
     async do_fetch (url, opts) {
         let token;
         const try_fetch = async () => {

AMRC-FactoryPlus/amrc-connectivity-stack

lib/service/fetch.js
@@ -4,9 +4,9 @@
  * Copyright 2022 AMRC.
  */
 
-import { build_node_fetch, GSS } from "../deps.js";
+import { build_node_fetch, GSS, WebSocket } from "../deps.js";
 
-import { ServiceInterface } from "./service-interface.js";
+import { ServiceError, ServiceInterface } from "./service-interface.js";
 
 const Auth_rx = /^([A-Za-z]+) +([A-Za-z0-9._~+/=-]+)$/;
 
@@ -101,6 +101,58 @@ export class Fetch extends ServiceInterface {
         }
     }
 
+    async websocket (opts) {
+        const base = opts.service_url
+            ?? await this.fplus.service_url(opts.service);
+        const url = new URL(opts.url, base);
+
+        const ws = new WebSocket(url);
+        await new Promise((resolve, reject) => {
+            const errh = e => {
+                const msg = e.type == "error"
+                    ? `WebSocket connection error: ${e.message}`
+                    : "WebSocket connection failed";
+                /* XXX should supply e as cause here */
+                reject(new ServiceError(opts.service, msg));
+            };
+            ws.addEventListener("open", resolve, { once: true });
+            ws.addEventListener("error", errh, { once: true });
+        });
+        return ws;
+    }
+
+    async ws_with_auth (opts) {
+        const base = await this.fplus.service_url(opts.service);
+
+        const try_ws = async bad => {
+            const ws = await this.websocket({ ...opts, service_url: base });
+            const token = await this.service_token(base, bad);
+
+            ws.send(`Bearer ${token}`);
+            const msg = await new Promise((resolve, reject) => {
+                ws.addEventListener("message", resolve, { once: true });
+                ws.addEventListener("error", reject, { once: true });
+            });
+
+            return [ws, msg.data, token];
+        };
+
+        let [ws, st, bad] = await try_ws();
+        if (st == "401")
+            [ws, st] = await try_ws(bad);
+        if (st == "200")
+            return ws;
+        
+        if (typeof st == "string" && /^[0-9]{3}$/.test(st)) {
+            throw new ServiceError(opts.service,
+                "WebSocket auth refused",
+                Number.parseInt(st, 10));
+        }
+
+        throw new ServiceError(opts.service,
+            "Unrecognised WS auth response: %s", st);
+    }
+        
     async do_fetch (url, opts) {
         let token;
         const try_fetch = async () => {
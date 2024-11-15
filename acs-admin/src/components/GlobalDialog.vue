5635266171b4a371d436ebc0a1f14ddfadb6c4c0
Alex Godbehere <alex.godbehere@gmail.com>
2024-11-15 15:57:28 +0000

    Implement global dialog composable

---
+++ acs-admin/src/components/GlobalDialog.vue
@@ -0,0 +1,87 @@
+<!--
+  - Copyright (c) University of Sheffield AMRC 2024.
+  -->
+
+<template>
+  <template>
+  <AlertDialog v-model:open="open">
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>{{title}}</AlertDialogTitle>
+        <AlertDialogDescription>
+          {{message}}
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel @click="cancel">{{cancelText}}</AlertDialogCancel>
+        <AlertDialogAction @click="confirm">{{confirmText}}</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
+</template>
+
+<script>
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
+
+export default {
+
+  components: {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+    confirmText: {
+      type: String,
+      default: 'Confirm',
+    },
+    cancelText: {
+      type: String,
+      default: 'Cancel',
+    },
+    onConfirm: {
+      type: Function,
+      default: null,
+    },
+    onCancel: {
+      type: Function,
+      default: null,
+    },
+  },
+  data () {
+    return {
+      open: true,
+    }
+  },
+  methods: {
+    confirm () {
+      if (this.onConfirm) this.onConfirm()
+      this.close()
+    },
+    cancel () {
+      if (this.onCancel) this.onCancel()
+      this.close()
+    },
+    close () {
+      this.open = false
+      this.$emit('close')
+    },
+  },
+}
+</script>
---
+++ acs-admin/src/components/ui/alert-dialog/AlertDialogDescription.vue
@@ -0,0 +1,30 @@
+<!--
+  - Copyright (c) University of Sheffield AMRC 2024.
+  -->
+
+<script setup>
+import { cn } from '@/lib/utils'
+import { AlertDialogDescription } from 'radix-vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
+
+<template>
+  <AlertDialogDescription
+    v-bind="delegatedProps"
+    :class="cn('text-sm text-slate-500 dark:text-slate-400', props.class)"
+  >
+    <slot />
+  </AlertDialogDescription>
+</template>

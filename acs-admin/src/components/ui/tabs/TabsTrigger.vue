80d350158bd63e195c80fd2ec58dc447061d4efc
Alex Godbehere <alex.godbehere@gmail.com>
2024-11-15 11:52:50 +0000

    Move principals to tab on AccessControl

---
+++ acs-admin/src/components/ui/tabs/TabsTrigger.vue
@@ -0,0 +1,41 @@
+<!--
+  - Copyright (c) University of Sheffield AMRC 2024.
+  -->
+
+<script setup>
+import { cn } from '@/lib/utils'
+import { TabsTrigger, useForwardProps } from 'radix-vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  value: { type: [String, Number], required: true },
+  disabled: { type: Boolean, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+
+const forwardedProps = useForwardProps(delegatedProps);
+</script>
+
+<template>
+  <TabsTrigger
+    v-bind="forwardedProps"
+    :class="
+      cn(
+        'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-950 data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50',
+        props.class,
+      )
+    "
+  >
+    <span class="truncate">
+      <slot />
+    </span>
+  </TabsTrigger>
+</template>

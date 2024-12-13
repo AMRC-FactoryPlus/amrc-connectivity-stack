46b0de56156ff9217348f3bf19b913b193146b18
Ben Morrow <b.morrow@amrc.co.uk>
2024-12-13 15:11:47 +0000

    Move over to `service-client`

--- acs-service-setup/lib/fixups.js
+++ acs-service-setup/lib/fixups.js
@@ -3,7 +3,7 @@
  * Copyright 2023 AMRC
  */
 
-import { UUIDs } from "@amrc-factoryplus/utilities";
+import { UUIDs } from "@amrc-factoryplus/service-client";
 
 import { ACS, Clusters, Fixup } from "./uuids.js";
 

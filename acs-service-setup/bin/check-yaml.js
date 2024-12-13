144d93b2eee6932fae547a44dece66bb30e89639
Ben Morrow <b.morrow@amrc.co.uk>
2024-12-13 15:12:46 +0000

    Check ConfigDB dumps against the schema
    This means loading the schema file from the acs-configdb directory.

--- acs-service-setup/bin/check-yaml.js
+++ acs-service-setup/bin/check-yaml.js
@@ -1,14 +1,40 @@
 import fsp from "fs/promises";
 import process from "process";
+import util from "util";
+
+import Ajv from "ajv/dist/2020.js";
+import ajv_formats from "ajv-formats";
+
+import { UUIDs } from "@amrc-factoryplus/service-client";
+
 import {load_yaml} from "../lib/dumps.js";
+
+const { dump_schema } = await import("../../acs-configdb/lib/dump-schema.js")
+    .catch(e => null);
+if (!dump_schema)
+    console.log("!!! Cannot load ConfigDB dump schema");
+
+const ajv = new Ajv();
+ajv_formats(ajv);
+
+const cdb_validate = dump_schema ? ajv.compile(dump_schema) : () => true;
 
 const files = await fsp.readdir("dumps");
 let exit = 0;
+
 for (const f of files) {
     if (!f.endsWith(".yaml"))
         continue;
     try { 
-        await load_yaml(f);
+        const ds = await load_yaml(f);
+        ds.filter(d => d.service == UUIDs.Service.ConfigDB)
+            .forEach(d => {
+                if (!cdb_validate(d)) {
+                    throw new Error(`Schema validation failed for ${f}`, { 
+                        cause: cdb_validate.errors.map(util.inspect),
+                    });
+                }
+            });
     } 
     catch (e) {
         exit = 1;

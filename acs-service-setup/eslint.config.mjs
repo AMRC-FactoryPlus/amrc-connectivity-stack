733c5bf77224d92cb8de6574419fc7200f0a7031
Ben Morrow <b.morrow@amrc.co.uk>
2024-12-13 15:20:21 +0000

    Update eslint

--- acs-service-setup/eslint.config.mjs
+++ acs-service-setup/eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
                 args:           "none",
                 caughtErrors:   "none",
             }],
-                
+            "no-fallthrough": "off",                
         },
     },
 ];

2db0adb9a0e142dad3ab353e2319d8f3bc2e0041
Alex Godbehere <alex.godbehere@gmail.com>
2025-01-20 14:11:13 +0000

    Add missing PermissionList.vue

---
+++ acs-admin/src/pages/AccessControl/Permissions/permissionListColumns.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) University of Sheffield AMRC 2025.
+ */
+
+import type {ColumnDef} from '@tanstack/vue-table'
+import {h} from 'vue'
+
+import DataTableColumnHeader from '@/components/ui/data-table/DataTableColumnHeader.vue'
+
+export interface Permission {
+    uuid: string
+    name: string
+}
+
+export const columns: ColumnDef<Permission>[] = [
+    {
+        accessorKey: 'name',
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Name'
+        }),
+
+        cell: ({row}) => {
+            return h('span', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('name'))
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        },
+    },
+    {
+        accessorKey: 'uuid',
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'UUID'
+        }),
+
+        cell: ({row}) => {
+            return h('span', {class: 'max-w-[500px] truncate text-gray-500'}, row.getValue('uuid'))
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        },
+    },
+    // {
+    //     id: 'actions',
+    //     cell: ({row}) => {
+    //         return h('i', {class: 'fa-solid fa-chevron-right'})
+    //     },
+    // }
+    ]

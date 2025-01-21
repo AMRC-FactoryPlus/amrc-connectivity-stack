dcb8579b4f2ee2fe6aa844cdc23d865959ec74eb
Kallum Burgin <Kallb123@users.noreply.github.com>
2025-01-21 14:45:34 +0000

    Implement Permissions tab on Principal page on Admin UI

---
+++ acs-admin/src/pages/AccessControl/Principals/permissionColumns.ts
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) University of Sheffield AMRC 2024.
+ */
+
+import type {ColumnDef} from '@tanstack/vue-table'
+import {h} from 'vue'
+
+import DataTableColumnHeader from '@/components/ui/data-table/DataTableColumnHeader.vue'
+import GroupDropdown from './GroupDropdown.vue'
+
+export interface Permission {
+    permission: {
+        uuid: string,
+        name: string,
+    },
+    target: {
+        uuid: string,
+        name: string,
+    },
+}
+
+export const columns: ColumnDef<Permission>[] = [
+    {
+        accessorKey: 'permission',
+        accessorFn: (row) => row.permission.name,
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Permission Name'
+        }),
+
+        cell: ({row}) => {
+            // return h('span', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('permission')) // row.original.permission.name
+            return h('div', {class: 'max-w-[500px] truncate'}, [
+                h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('permission')),
+                h('div', {class: 'max-w-[500px] truncate text-gray-400'}, row.original.permission.uuid)
+            ])
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        },
+    },
+    {
+        accessorKey: 'target',
+        accessorFn: (row) => row.target.name,
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Target Name'
+        }),
+
+        cell: ({row}) => {
+            // return h('span', {class: 'max-w-[500px] truncate text-gray-500'}, row.getValue('target'))
+            return h('div', {class: 'max-w-[500px] truncate'}, [
+                h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('target')),
+                h('div', {class: 'max-w-[500px] truncate text-gray-400'}, row.original.target.uuid)
+            ])
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        },
+    },
+    {
+        id: 'actions',
+        cell: ({row}) => h(GroupDropdown, {row}),
+    }]

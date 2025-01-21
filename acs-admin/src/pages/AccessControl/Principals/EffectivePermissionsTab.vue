e05e4827d7027903e0a1d950738b329cc2383d8d
Kallum Burgin <Kallb123@users.noreply.github.com>
2025-01-21 21:48:37 +0000

    Fix errors when clicking on rows with multiple entities
    We will need to implement a method to specify specifically which entity is being clicked

--- acs-admin/src/pages/AccessControl/Principals/EffectivePermissionsTab.vue
+++ acs-admin/src/pages/AccessControl/Principals/EffectivePermissionsTab.vue
@@ -4,7 +4,8 @@
 
 <template>
   <Skeleton v-if="loading" v-for="i in 10" class="h-16 rounded-lg mb-2"/>
-  <DataTable v-else :data="permissions" :columns="columns" :filters="[]" @row-click="e => $emit('objectClick', e)">
+<!--  <DataTable v-else :data="permissions" :columns="columns" :filters="[]" @row-click="e => $emit('objectClick', e)">-->
+  <DataTable v-else :data="permissions" :columns="columns" :filters="[]">
     <template #toolbar-left>
       <Alert class="mr-6">
         <div class="flex items-start gap-3">

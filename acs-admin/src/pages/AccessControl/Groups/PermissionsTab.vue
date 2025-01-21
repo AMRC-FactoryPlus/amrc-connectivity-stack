4fb11e6d98d3abb3e1b0a6bf040234bdb0d0bd04
Kallum Burgin <Kallb123@users.noreply.github.com>
2025-01-21 16:08:05 +0000

    Add Permissions tab to Group sidebar on Admin UI

---
+++ acs-admin/src/pages/AccessControl/Groups/PermissionsTab.vue
@@ -0,0 +1,114 @@
+<!--
+  - Copyright (c) University of Sheffield AMRC 2024.
+  -->
+
+<template>
+  <Skeleton v-if="loading || p.loading" v-for="i in 10" class="h-16 rounded-lg mb-2"/>
+  <DataTable v-else :data="this.permissions" :columns="columns" :filters="[]">
+    <template #toolbar-left>
+      <Alert class="mr-6">
+        <div class="flex items-start gap-3">
+          <i class="fa-solid fa-circle-info mt-1"></i>
+          <div class="flex flex-col">
+            <AlertTitle>Permissions</AlertTitle>
+            <AlertDescription>
+              Manage the permissions that are assigned to this group.
+            </AlertDescription>
+          </div>
+        </div>
+      </Alert>
+    </template>
+  </DataTable>
+</template>
+
+<script>
+import DataTable from '@components/ui/data-table/DataTable.vue'
+import { Skeleton } from '@components/ui/skeleton/index.js'
+import { columns } from './permissionColumns.ts'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { usePermissionStore } from "@store/usePermissionStore.js";
+import { UUIDs } from "@amrc-factoryplus/service-client";
+import { useServiceClientStore } from "@store/serviceClientStore.js";
+
+export default {
+  setup () {
+    return {
+      columns,
+      p: usePermissionStore(),
+      s: useServiceClientStore(),
+    }
+  },
+
+  components: {
+    DataTable,
+    Skeleton,
+    Alert,
+    AlertTitle,
+    AlertDescription,
+  },
+
+  props: {
+    group: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  watch: {
+    group: {
+      handler (val) {
+        if (val == null) {
+          return
+        }
+
+        this.fetchSpecificPermissions(val.uuid)
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    async fetchSpecificPermissions (uuid) {
+      this.loading = true
+
+      const res = await this.s.client.Auth.fetch(`/authz/ace`)
+      if (!Array.isArray(res[1])) {
+        return;
+      }
+      const fullList = res[1];
+      const filteredList = fullList.filter(e => e.principal === uuid)
+
+      const info = o => this.s.client.ConfigDB.get_config(UUIDs.App.Info, o)
+      const name = o => info(o).then(v => v?.name)
+
+      const rv = []
+      for (const entry of filteredList) {
+        const permissionName = this.p.data.find(v => v.uuid === entry.permission)?.name ?? await name(entry.permission)
+        const targetName     = await name(entry.target)
+
+        rv.push({
+          permission: {
+            uuid: entry.permission,
+            name: permissionName,
+          },
+          target: {
+            uuid: entry.target,
+            name: targetName,
+          },
+        })
+      }
+
+      this.permissions = rv
+      this.loading     = false
+    },
+  },
+
+  data () {
+    return {
+      loading: false,
+      permissions: []
+    }
+  },
+
+}
+</script>
---
+++ acs-admin/src/pages/AccessControl/Groups/permissionColumns.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) University of Sheffield AMRC 2024.
+ */
+
+import type {ColumnDef} from '@tanstack/vue-table'
+import {h} from 'vue'
+import {Badge} from '@/components/ui/badge'
+
+import DataTableColumnHeader from '@/components/ui/data-table/DataTableColumnHeader.vue'
+import GroupDropdown from './GroupDropdown.vue'
+
+export interface Permission {
+    permission: {
+        uuid: string,
+        name: string,
+    },
+    target: {
+        uuid: string,
+        name: string,
+    },
+}
+
+export const columns: ColumnDef<Permission>[] = [
+    {
+        accessorKey: 'permission',
+        accessorFn: (row) => row.permission.name,
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Permission Name'
+        }),
+
+        cell: ({row}) => {
+            return h('div', {class: 'max-w-[500px] truncate'}, [
+                h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('permission')),
+                h('div', {class: 'max-w-[500px] truncate text-gray-400'}, row.original.permission.uuid)
+            ])
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        },
+    },
+    {
+        accessorKey: 'target',
+        accessorFn: (row) => row.target.name,
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Target Name'
+        }),
+
+        cell: ({row}) => {
+            if (row.original.target.uuid === '00000000-0000-0000-0000-000000000000') {
+                return h(Badge, {variant: 'destructive'}, {
+                    default: () => row.original.target.name
+                });
+            }
+            return h('div', {class: 'max-w-[500px] truncate'}, [
+                h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('target')),
+                h('div', {class: 'max-w-[500px] truncate text-gray-400'}, row.original.target.uuid)
+            ])
+        },
+        filterFn: (row, id, value) => {
+            return value.includes(row.getValue(id))
+        },
+    },
+    {
+        id: 'actions',
+        cell: ({row}) => h(GroupDropdown, {row}),
+    }]

39a884e2efc6be9a24f6532d0baa3063bed3751b
Kallum Burgin <Kallb123@users.noreply.github.com>
2025-01-22 11:32:54 +0000

    Fix actions button firing row event on admin UI

--- acs-admin/src/pages/AccessControl/Principals/GroupDropdown.vue
+++ acs-admin/src/pages/AccessControl/Principals/GroupDropdown.vue
@@ -46,15 +46,17 @@ function handleDelete() {
 
 <template>
   <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button
-          variant="ghost"
-          class="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-      >
-        <i class="fa-solid fa-ellipsis"></i>
-        <span class="sr-only">Open menu</span>
-      </Button>
-    </DropdownMenuTrigger>
+    <div @click.stop>
+      <DropdownMenuTrigger as-child>
+        <Button
+            variant="ghost"
+            class="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+        >
+          <i class="fa-solid fa-ellipsis"></i>
+          <span class="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+    </div>
     <DropdownMenuContent align="end" class="w-[160px]">
       <DropdownMenuGroup>
         <DropdownMenuItem @click="copy(props.row.original.uuid)">

AMRC-FactoryPlus/amrc-connectivity-stack

commit e647cfa069d6b09026a3b6551534accc3f0886d8

Apply ACLs to the notifications

acs-configdb/lib/model.js
@@ -359,7 +359,7 @@ export default class Model extends EventEmitter {
             return await special.get(q.object);
 
         let dbr = await this.db.query(`
-            select c.json, c.etag
+            select c.json config, c.etag
             from config c
                 join object a on a.id = c.app
                 join object o on o.id = c.object

acs-configdb/bin/api.js
@@ -15,6 +15,7 @@ import { WebAPI }           from "@amrc-factoryplus/service-api";
 import { GIT_VERSION } from "../lib/git-version.js";
 import { Service, Version } from "../lib/constants.js";
 
+import { Auth } from "../lib/auth.js";
 import Model from "../lib/model.js";
 import APIv1 from "../lib/api-v1.js";
 import MQTTCli from "../lib/mqttcli.js";
@@ -27,9 +28,9 @@ const model = await new Model({
     log: fplus.debug.bound("model"),
 }).init();
 
+const auth = new Auth({ fplus });
 const api_v1 = new APIv1({
-    fplus_client:   fplus,
-    model,
+    auth, model, fplus
 });
 
 const api = await new WebAPI({
@@ -64,7 +65,7 @@ const api = await new WebAPI({
 }).init();
 
 const notify = new Notify({
-    api, model,
+    auth, api, model,
     log:    fplus.debug.bound("notify"),
 });
 

acs-configdb/lib/api-v1.js
@@ -10,16 +10,7 @@ import { UUIDs } from "@amrc-factoryplus/service-client";
 
 import * as etags from "./etags.js";
 
-import {App, Class} from "./constants.js";
-
-const Perm = {
-    All: "c43c7157-a50b-4d2a-ac1a-86ff8e8e88c1",
-    Read_App: "4a339562-cd57-408d-9d1a-6529a383ea4b",
-    Write_App: "6c799ccb-d2ad-4715-a2a7-3c8728d6c0bf",
-    Manage_Obj: "f0b7917b-d475-4888-9d5a-2af96b3c26b6",
-    Delete_Obj: "6957174b-7b08-45ca-ac5c-c03ab6928a6e",
-    Manage_App: "95c7cbcb-ce60-49ed-aa81-2fe3eec4559d",
-};
+import {App, Class, Perm} from "./constants.js";
 
 function compat(dest) {
     const pieces = dest.split("/");
@@ -35,7 +26,8 @@ function compat(dest) {
 
 export default class API {
     constructor(opts) {
-        this.fplus = opts.fplus_client;
+        this.fplus = opts.fplus;
+        this.auth = opts.auth;
 
         this.log = this.fplus.debug.bound("apiv1");
 
@@ -98,13 +90,8 @@ export default class API {
         api.get("/save", this.dump_save.bind(this));
     }
 
-    async _check_acl(principal, permission, target, wild) {
-        const acl = await this.fplus.fetch_acl(principal, Perm.All);
-        return acl(permission, target, wild);
-    }
-
     async apps_get(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, Class.App, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, Class.App, true);
         if (!ok) return res.status(403).end();
 
         let list = await this.model.apps_get();
@@ -112,7 +99,7 @@ export default class API {
     }
 
     async apps_post(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, Class.App, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, Class.App, true);
         if (!ok) return res.status(403).end();
 
         const uuid = req.body.uuid;
@@ -131,7 +118,7 @@ export default class API {
     }
 
     async app_get(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, Class.App, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, Class.App, true);
         if (!ok) return res.status(403).end();
 
         const uuid = req.params.app;
@@ -141,7 +128,7 @@ export default class API {
 
         const info = await this.model.config_get(
             {app: App.Info, object: uuid});
-        const name = info?.json?.name ?? "";
+        const name = info?.config?.name ?? "";
 
         res.status(200).json({uuid, name});
     }
@@ -149,7 +136,7 @@ export default class API {
     async app_schema_get(req, res) {
         const app = req.params.app;
 
-        const ok = await this._check_acl(req.auth, Perm.Read_App, app, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Read_App, app, true);
         if (!ok) return res.status(403).end();
 
         let rv = await this.model.app_schema(app);
@@ -162,7 +149,7 @@ export default class API {
     async app_schema_put(req, res) {
         const app = req.params.app;
 
-        const ok = await this._check_acl(req.auth, Perm.Manage_App, app, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_App, app, true);
         if (!ok) return res.status(403).end();
 
         let rv = await this.model.app_schema_update(app, req.body);
@@ -178,7 +165,7 @@ export default class API {
     }
 
     async object_list(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, UUIDs.Null);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, UUIDs.Null);
         if (!ok) return res.status(403).end();
 
         let list = await this.model.object_list();
@@ -188,7 +175,7 @@ export default class API {
     async object_create(req, res) {
         let {uuid, "class": klass} = req.body;
 
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, klass, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, klass, true);
         if (!ok) return res.status(403).end();
 
         let st;
@@ -207,7 +194,7 @@ export default class API {
     async object_delete(req, res) {
         const {object} = req.params;
 
-        const ok = await this._check_acl(req.auth, Perm.Delete_Obj, object, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Delete_Obj, object, true);
         if (!ok) return res.status(403).end();
 
         const [st, rv] = await this.model.object_delete(object);
@@ -217,7 +204,7 @@ export default class API {
     }
 
     async class_list(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, UUIDs.Null);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, UUIDs.Null);
         if (!ok) return res.status(403).end();
 
         const list = await this.model.class_list();
@@ -226,7 +213,7 @@ export default class API {
 
     async class_get(req, res) {
         const klass = req.params.class;
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, klass, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, klass, true);
         if (!ok) return res.status(403).end();
 
         const list = await this.model.class_get(klass);
@@ -238,7 +225,7 @@ export default class API {
     async config_list(req, res) {
         let {app, "class": klass} = req.params;
 
-        const ok = await this._check_acl(req.auth, Perm.Read_App, app, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Read_App, app, true);
         if (!ok) return res.status(403).end();
 
         let list = klass
@@ -252,7 +239,7 @@ export default class API {
     }
 
     async config_get(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Read_App,
+        const ok = await this.auth.check_acl(req.auth, Perm.Read_App,
             req.params.app, true);
         if (!ok) return res.status(403).end();
 
@@ -268,11 +255,11 @@ export default class API {
 
         res.status(200);
         if (entry.etag) res.header("ETag", `"${entry.etag}"`);
-        res.json(entry.json);
+        res.json(entry.config);
     }
 
     async config_put(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Write_App,
+        const ok = await this.auth.check_acl(req.auth, Perm.Write_App,
             req.params.app, true);
         if (!ok) return res.status(403).end();
 
@@ -288,7 +275,7 @@ export default class API {
     }
 
     async config_delete(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Write_App,
+        const ok = await this.auth.check_acl(req.auth, Perm.Write_App,
             req.params.app, true);
         if (!ok) return res.status(403).end();
 
@@ -304,7 +291,7 @@ export default class API {
 
         const ok = await Promise.all(
             [Perm.Read_App, Perm.Write_App]
-            .map(p => this._check_acl(req.auth, p, req.params.app, true)));
+            .map(p => this.auth.check_acl(req.auth, p, req.params.app, true)));
         if (ok.includes(false))
             return res.status(403).end();
 
@@ -342,7 +329,7 @@ export default class API {
 
     async config_search(req, res) {
         const {app, "class": klass} = req.params;
-        const ok = await this._check_acl(req.auth, Perm.Read_App, app, true);
+        const ok = await this.auth.check_acl(req.auth, Perm.Read_App, app, true);
         if (!ok) return res.status(403).end();
 
         const query = this.config_search_parse(req.query);
@@ -380,7 +367,7 @@ export default class API {
         };
         for (const [key, perm] of Object.entries(perms)) {
             if (key in dump) {
-                const ok = await this._check_acl(
+                const ok = await this.auth.check_acl(
                     req.auth, perm, UUIDs.Null, false);
                 if (!ok) {
                     this.log("Refusing dump (%s)", key);
@@ -394,8 +381,8 @@ export default class API {
     }
 
     async dump_save(req, res) {
-        const ok = await this._check_acl(req.auth, Perm.Manage_Obj, UUIDs.Null)
-            && await this._check_acl(req.auth, Perm.Read_App, UUIDs.Null);
+        const ok = await this.auth.check_acl(req.auth, Perm.Manage_Obj, UUIDs.Null)
+            && await this.auth.check_acl(req.auth, Perm.Read_App, UUIDs.Null);
         if (!ok) return res.status(403).end();
 
         const dump = await this.model.dump_save();

acs-configdb/lib/auth.js
@@ -1,0 +1,18 @@
+/*
+ * ACS ConfigDB
+ * ACL handling
+ * Copyright 2024 University of Sheffield
+ */
+
+import { Perm } from "./constants.js";
+
+export class Auth {
+    constructor (opts) {
+        this.auth = opts.fplus.Auth;
+    }
+
+    async check_acl(principal, permission, target, wild) {
+        const acl = await this.auth.fetch_acl(principal, Perm.All);
+        return acl(permission, target, wild);
+    }
+}

acs-configdb/lib/constants.js
@@ -39,3 +39,13 @@ export const Device_Info = {
     Model: "AMRC Connectivity Stack (ACS) ConfigDB",
     Serial: GIT_VERSION,
 };
+
+export const Perm = {
+    All: "c43c7157-a50b-4d2a-ac1a-86ff8e8e88c1",
+    Read_App: "4a339562-cd57-408d-9d1a-6529a383ea4b",
+    Write_App: "6c799ccb-d2ad-4715-a2a7-3c8728d6c0bf",
+    Manage_Obj: "f0b7917b-d475-4888-9d5a-2af96b3c26b6",
+    Delete_Obj: "6957174b-7b08-45ca-ac5c-c03ab6928a6e",
+    Manage_App: "95c7cbcb-ce60-49ed-aa81-2fe3eec4559d",
+};
+

acs-configdb/lib/notify-v2.js
@@ -9,6 +9,7 @@ import rx from "rxjs";
 import { WebSocketServer } from "ws";
 import { v4 as uuidv4 } from "uuid";
 
+import { Perm } from "./constants.js";
 import * as rxu from "./rx-util.js";
 
 const Token_rx = /^Bearer ([A-Za-z0-9+/]+)$/;
@@ -19,24 +20,34 @@ function register_handler (path, handler) {
     Handlers.push([pathToRegexp(path), handler]);
 }
 
-function single_config (model, app, object) {
-    return rxu.rx(
-        rx.concat(
-            rxu.rx(
-                model.config_get({ app, object }),
-                rx.map(r => ({
-                    status:     r ? 200 : 404,
-                    body:       r?.config,
-                })),
-                rx.map(response => ({ status: 201, response }))),
-            rxu.rx(
-                model.updates,
-                rx.filter(u => u.app == app && u.object == object),
-                rx.map(u => ({
-                    status: u.config ? 200 : 404,
-                    body:   u.config,
-                })),
-                rx.map(response => ({ status: 200, response })))));
+function single_config (session, app, object) {
+    const to_response = async upd => {
+        session.log("Map update: %o", upd);
+        const ok = await session.check_acl(Perm.Read_App, app, true);
+        if (!ok)
+            return { status: 403 };
+        if (!upd?.config)
+            return { status: 404 };
+        const res = { status: 200, body: upd.config };
+        if (upd.etag)
+            res.headers = { etag: upd.etag };
+        return res;
+    };
+
+    /* XXX Strictly there is a race condition here: the initial fetch
+     * does not slot cleanly into the sequence of updates. This would be
+     * difficult to fix. */
+    return rx.concat(
+        rxu.rx(
+            session.model.config_get({ app, object }),
+            rx.flatMap(to_response),
+            rx.map(response => ({ status: 201, response }))),
+        rxu.rx(
+            session.model.updates,
+            rx.filter(u => u.app == app && u.object == object),
+            rx.flatMap(to_response),
+            rx.map(response => ({ status: 200, response }))),
+    );
 }
 register_handler("app/:app/object/:object", single_config);
 
@@ -44,7 +55,8 @@ class Session {
     constructor (opts) {
         this.ws = opts.ws;
 
-        this.fpauth = opts.notify.api.auth;
+        this.authn = opts.notify.api.auth;
+        this.authz = opts.notify.auth;
         this.log = (m, ...a) => 
             opts.notify.log(`[%s] ${m}`, this.uuid, ...a);
         this.model = opts.notify.model;
@@ -82,7 +94,7 @@ class Session {
         if (!creds)
             return fail("400", "Bad auth message");
 
-        const princ = await this.fpauth.auth_bearer({ creds: creds[1] })
+        const princ = await this.authn.auth_bearer({ creds: creds[1] })
             .catch(e => { this.log(e); return null; });
         if (!princ)
             return fail("401", "WS auth failed");
@@ -93,9 +105,15 @@ class Session {
         return princ;
     }
 
-    build_updates () {
-        const { ws } = this;
+    check_acl (...args) {
+        if (!this.principal) {
+            this.log("Checking ACL with no principal");
+            return false;
+        }
+        return this.authz.check_acl(this.principal, ...args);
+    }
 
+    build_updates () {
         const [closes, opens] = rx.partition(
             this.requests(),
             r => r.method == "CLOSE");
@@ -159,7 +177,7 @@ class Session {
         for (const [rxp, handler] of Handlers) {
             const match = rxp.exec(req.url);
             if (!match) continue;
-            return handler(this.model, ...match.slice(1));
+            return handler(this, ...match.slice(1));
         }
         return fail(404);
     }
@@ -184,6 +202,7 @@ export class Notify {
         this.log = opts.log;
         this.model = opts.model;
         this.api = opts.api;
+        this.auth = opts.auth;
     }
 
     run () {

commit 88a0073b8e3ae6906b5787ad719b0c70f3565007

ConfigDB PATCH was destroying old content
This is a bug introduced in e647cfa when changing the structure of
config_get to match the other notification sources.

acs-configdb/lib/model.js
@@ -475,7 +475,7 @@ export default class Model extends EventEmitter {
 
         /* This needs to be null, not undefined, for a nonexistent
          * object, or the 409 test below will fail. */
-        const json = conf?.json ?? null;
+        const json = conf?.config ?? null;
 
         /* Safety check: applying a merge-patch to a non-object destroys
          * the whole thing. */
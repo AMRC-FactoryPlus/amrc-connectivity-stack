b5c31985e7479d183dec77b02eb69d7dcc9b67f2
Ben Morrow <b.morrow@amrc.co.uk>
2025-01-28 10:46:57 +0000

    Filter permissions by requester ACL
    This applies the Read_ACL meta-permission.

--- acs-auth/lib/api_v2.js
+++ acs-auth/lib/api_v2.js
@@ -5,6 +5,7 @@
  */
 
 import express from "express";
+import * as imm from "immutable";
 import * as rx from "rxjs";
 
 import { UUIDs } from "@amrc-factoryplus/service-client";
@@ -12,6 +13,10 @@ import { UUIDs } from "@amrc-factoryplus/service-client";
 import Model from "./model.js";
 import {Perm} from "./uuids.js";
 import { booleans, valid_krb, valid_uuid } from "./validate.js";
+
+function fail (status) {
+    throw { status };
+}
 
 export class APIv2 {
     constructor(opts) {
@@ -31,16 +36,40 @@ export class APIv2 {
         return api;
     }
 
-    async get_acl (req, res) {
-        /* XXX No auth for now */
-        const { principal } = req.params;
-
-        this.log("Fetching ACL for %s", principal);
-        const acl = await rx.firstValueFrom(
+    async fetch_acl (principal) {
+        const acl = rx.firstValueFrom(
             this.data.acl_for(principal));
-        this.log("Got ACL for %s", principal);
+        this.log("Fetched ACL for %s: %o", principal, acl);
+        return acl;
+    }
 
-        if (!acl) return res.status(404).end();
-        return res.status(200).json(acl);
+    async check_acl (krb, perm, targ, wild) {
+        const princ = await this.model.principal_find_by_krb(krb);
+        if (!princ) fail(403);
+
+        const acl = await this.fetch_acl(princ);
+        throw "incomplete";
+    }
+
+    async get_acl (req, res) {
+        const { principal } = req.params;
+        if (!valid_uuid(principal)) fail(410);
+
+        const acl = await this.fetch_acl(principal);
+        if (!acl) fail(404);
+
+        const permitted = await this.model.principal_find_by_krb(req.auth)
+            .then(r => this.fetch_acl(r))
+            .then(acl => imm.Seq(acl)
+                .filter(e => e.permission == Perm.Read_ACL)
+                .map(e => e.target)
+                .toSet());
+        this.log("Permitted Read_ACL for %s: %o", req.auth, permitted.toJS());
+
+        const rv = permitted.has(UUIDs.Special.Null) ? acl
+            : acl.filter(e => permitted.has(e.permission));
+        this.log("Returning ACL %o", rv);
+
+        return res.status(200).json(rv);
     }
 }

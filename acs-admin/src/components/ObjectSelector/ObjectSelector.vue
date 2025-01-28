69a2e48813ae9511867ef9dbbb431feedae43cb1
Kallum Burgin <Kallb123@users.noreply.github.com>
2025-01-28 14:06:06 +0000

    Add optional height limiter to object selector

--- acs-admin/src/components/ObjectSelector/ObjectSelector.vue
+++ acs-admin/src/components/ObjectSelector/ObjectSelector.vue
@@ -21,7 +21,7 @@
         </div>
       </DialogHeader>
       <div class="flex flex-col justify-center gap-6 overflow-auto flex-1 fix-inset">
-        <DataTableSearchable v-if="columns" :data="storeData" :search-key="titleKey" :columns="columns" :filters="[]">
+        <DataTableSearchable v-if="columns" :data="storeData" :search-key="titleKey" :columns="columns" :limit-height="true" :filters="[]">
           <template #default="slotProps">
             <Button @click="() => {$emit('update:modelValue', slotProps.selectedUsers); showModal = false}" >Confirm Selection</Button>
           </template>

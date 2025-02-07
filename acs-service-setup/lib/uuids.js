936f3c5386d531fb1432beead654d9a4cf75bc4d
D J Newbould <D.J.Newbould@sheffield.ac.uk>
2025-02-07 15:48:03 +0000

    Migrated CmdEsc registration to service setup

--- acs-service-setup/lib/uuids.js
+++ acs-service-setup/lib/uuids.js
@@ -60,6 +60,7 @@ export const ACS = {
         Manager:                "619eecab-742d-4824-8b97-bcae472e5c04",
         ClusterManager:         "2706aa43-a826-441e-9cec-cd3d4ce623c2",
         ConfigDB:               "af15f175-78a0-4e05-97c0-2a0bb82b9f3b",
+        CmdEsc:                 "78ea7071-24ac-4916-8351-aa3e549d8ccd",
     },
     Role: {
         EdgeNodeConsumer:       "17a64293-b82d-4db4-af4d-63359bb62934",
@@ -73,6 +74,7 @@ export const ACS = {
         ConfigDB:               "36861e8d-9152-40c4-8f08-f51c2d7e3c25",
         ClusterManager:         "127cde3c-773a-4f61-b0ba-7412a2695253",
         Directory:              "5cc3b068-938f-4bb2-8ceb-64338a02fbeb",
+        CmdEsc:                 "23d4e8f9-76c0-49d5-addc-00b6ac05ee58"
     },
     /* XXX These should not be fixed. They should be replaced by
      * per-deployment accounts created by krbkeys, and

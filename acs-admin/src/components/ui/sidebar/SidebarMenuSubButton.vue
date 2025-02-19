3b8fd6b39771f59ae03f9db6c7762d1b09c1e4ed
Alex Godbehere <alex.godbehere@gmail.com>
2025-02-19 09:38:19 +0000

    (admin) Migrate to ShadCN sidebar component for nav

---
+++ acs-admin/src/components/ui/sidebar/SidebarMenuSubButton.vue
@@ -0,0 +1,38 @@
+<!--
+  - Copyright (c) University of Sheffield AMRC 2025.
+  -->
+
+<script setup>
+import { cn } from '@/lib/utils'
+import { Primitive } from 'radix-vue'
+
+const props = defineProps({
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false, default: 'a' },
+  size: { type: String, required: false, default: 'md' },
+  isActive: { type: Boolean, required: false },
+  class: { type: null, required: false },
+});
+</script>
+
+<template>
+  <Primitive
+    data-sidebar="menu-sub-button"
+    :as="as"
+    :as-child="asChild"
+    :data-size="size"
+    :data-active="isActive"
+    :class="
+      cn(
+        'flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 text-sidebar-foreground outline-none ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground',
+        'data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground',
+        size === 'sm' && 'text-xs',
+        size === 'md' && 'text-sm',
+        'group-data-[collapsible=icon]:hidden',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </Primitive>
+</template>
---
+++ acs-admin/src/components/ui/sidebar/SidebarSeparator.vue
@@ -0,0 +1,21 @@
+<!--
+  - Copyright (c) University of Sheffield AMRC 2025.
+  -->
+
+<script setup>
+import { Separator } from '@/components/ui/separator'
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+  class: { type: null, required: false },
+});
+</script>
+
+<template>
+  <Separator
+    data-sidebar="separator"
+    :class="cn('mx-2 w-auto bg-sidebar-border', props.class)"
+  >
+    <slot />
+  </Separator>
+</template>

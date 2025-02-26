72c019becce66564e9363838afa3195a1e877e03
D J Newbould <D.J.Newbould@sheffield.ac.uk>
2025-02-26 16:49:26 +0000

    Move manager registration to service setup

--- acs-service-setup/lib/uuids.js
+++ acs-service-setup/lib/uuids.js
@@ -79,6 +79,7 @@ export const ACS = {
         MQTT:                   "2f42daeb-4521-4522-8e19-85dfb73db88e",
         Git:                    "626df296-8156-4c67-8aed-aac70161aa8b",
         Authentication:         "127cde3c-773a-4f61-b0ba-7412a2695253",
+        Manager:                "2340e706-1280-420c-84a6-016547b55e95",
     },
     /* XXX These should not be fixed. They should be replaced by
      * per-deployment accounts created by krbkeys, and

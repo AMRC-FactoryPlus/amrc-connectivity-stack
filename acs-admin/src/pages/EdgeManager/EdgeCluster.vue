900aca517d81efb07c1f658078c3c0e254cd4e14
Alex Godbehere <alex.godbehere@gmail.com>
2025-03-07 16:04:57 +0000

    Tweak edge cluster page styling

--- acs-admin/src/pages/EdgeManager/EdgeCluster.vue
+++ acs-admin/src/pages/EdgeManager/EdgeCluster.vue
@@ -5,61 +5,71 @@
 <template>
   <EdgeContainer>
     <div v-if="loadingDetails">LOADING</div>
-    <Tabs v-else default-value="details">
-      <div class="flex items-center justify-between gap-2">
-        <TabsList class="mb-6">
-          <TabsTrigger value="details">
-            Details
-          </TabsTrigger>
-          <TabsTrigger value="deployments" disabled>
-            Deployments
-          </TabsTrigger>
-          <TabsTrigger value="hosts" :disabled="cluster.hosts.length === 0">
-            {{cluster.hosts.length ? `${cluster.hosts.length} Host${cluster.hosts.length > 1 ? "s" : ""}` : "No Hosts"}}
-          </TabsTrigger>
-        </TabsList>
-      </div>
-      <TabsContent value="details">
-        <div>
-          <div>
-            <label for="clusterName" class="font-semibold">Cluster Name</label>
-            <div class="flex items-center gap-2">
-              <Input disabled name="clusterName" :default-value="cluster.name" />
-              <Button variant="ghost" @click="() => copy(cluster.name)">Copy</Button>
+    <div v-else class="flex flex-col gap-4">
+      <Card>
+        <CardHeader>
+          <div class="flex items-center justify-between gap-1">
+            <div class="flex flex-col gap-1">
+              <div class="flex mb-2">
+                <div class="px-1.5 py-0.5 rounded-md bg-green-600 text-sm font-bold uppercase tracking-wide text-white">
+                  Online
+                </div>
+              </div>
+              <Copyable :text="cluster.name">
+                <CardTitle class="text-2xl">
+                  {{cluster.name}}
+                </CardTitle>
+              </Copyable>
+              <Copyable :text="cluster.name">
+                <div class="flex items-center justify-center gap-1 text-sm text-gray-600 hover:text-gray-950 bg-gray-100 px-1.5 py-0.5 rounded-md">
+                  <i class="fa-solid fa-dharmachakra"></i>
+                  <div>{{cluster.helmChart.chart}}</div>
+                </div>
+              </Copyable>
+            </div>
+            <div class="flex bg-white items-center justify-between gap-2">
+              <Button size="sm" class="flex items-center justify-center gap-2">
+                <span class="hidden md:inline">Bootstrap</span>
+                <i class="fa-solid fa-rocket"></i>
+              </Button>
             </div>
           </div>
-          <div class="mt-4">
-            <label for="helmChart" class="font-semibold">Helm Chart</label>
-            <div class="flex items-center gap-2">
-              <Input disabled name="helmChart" :default-value="cluster.helmChart.chart" />
-              <Button variant="ghost" @click="() => copy(cluster.helmChart.chart)">Copy</Button>
-            </div>
-          </div>
+        </CardHeader>
+      </Card>
+      <Tabs default-value="nodes" class="flex flex-col">
+        <div class="flex items-center justify-between gap-2">
+          <TabsList>
+            <TabsTrigger value="nodes">
+              {{nodes.length ? `${nodes.length}  Node${nodes.length > 1 ? 's' : ''}` : 'No Nodes'}}
+            </TabsTrigger>
+            <TabsTrigger value="deployments" disabled>
+              Deployments
+            </TabsTrigger>
+            <TabsTrigger value="hosts" :disabled="cluster.hosts.length === 0">
+              {{cluster.hosts.length ? `${cluster.hosts.length} Host${cluster.hosts.length > 1 ? 's' : ''}` : 'No Hosts'}}
+            </TabsTrigger>
+          </TabsList>
         </div>
-        <div v-if="nodes.length > 0" class="mt-8">
-          <DataTable :data="nodes" :columns="nodeColumns" :filters="[]">
+        <TabsContent value="nodes">
+          <div v-if="nodes.length > 0">
+            <DataTable :data="nodes" :columns="nodeColumns" :filters="[]"/>
+          </div>
+        </TabsContent>
+        <TabsContent value="deployments">
+        </TabsContent>
+        <TabsContent value="hosts">
+          <DataTable :data="cluster.hosts" :columns="hostColumns" :filters="[]">
             <template #toolbar-left>
-              <div class="text-xl font-semibold">{{`${nodes.length} Node${nodes.length > 1 ? "s" : ""}`}}</div>
+              <div class="text-xl font-semibold">{{`${cluster.hosts.length} Host${cluster.hosts.length > 1 ? 's' : ''}`}}</div>
             </template>
           </DataTable>
-        </div>
-      </TabsContent>
-      <TabsContent value="deployments">
-      </TabsContent>
-      <TabsContent value="hosts">
-        <DataTable :data="cluster.hosts" :columns="hostColumns" :filters="[]">
-          <template #toolbar-left>
-            <div class="text-xl font-semibold">{{`${cluster.hosts.length} Host${cluster.hosts.length > 1 ? "s" : ""}`}}</div>
-          </template>
-        </DataTable>
-      </TabsContent>
-    </Tabs>
+        </TabsContent>
+      </Tabs>
     <template #header>
       <div class="flex bg-white items-center justify-between gap-2">
         <Button>Status</Button>
         <Button @click="copyBootstrap">Copy Bootstrap</Button>
-      </div>
-    </template>
+    </div>
   </EdgeContainer>
 </template>
 
@@ -68,24 +78,27 @@ import { useServiceClientStore } from '@store/serviceClientStore.js'
 import { UUIDs } from '@amrc-factoryplus/service-client'
 import { useNodeStore } from '@store/useNodeStore.js'
 import EdgeContainer from '@components/Containers/EdgeContainer.vue'
-import PermissionList from "@pages/AccessControl/Permissions/PermissionList.vue";
-import PrincipalList from "@pages/AccessControl/PrincipalList.vue";
+import PermissionList from '@pages/AccessControl/Permissions/PermissionList.vue'
+import PrincipalList from '@pages/AccessControl/PrincipalList.vue'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import LinkUserDialog from "@pages/AccessControl/LinkUserDialog.vue";
-import GroupList from "@pages/AccessControl/GroupList.vue";
-import {Label} from "@components/ui/label/index.js";
-import {Input} from "@components/ui/input/index.js";
-import {Card, CardContent} from "@components/ui/card/index.js";
-import {Button} from "@components/ui/button/index.js";
-import DataTable from "@components/ui/data-table/DataTable.vue";
+import LinkUserDialog from '@pages/AccessControl/LinkUserDialog.vue'
+import GroupList from '@pages/AccessControl/GroupList.vue'
+import { Label } from '@components/ui/label/index.js'
+import { Input } from '@components/ui/input/index.js'
+import { Button } from '@components/ui/button/index.js'
+import DataTable from '@components/ui/data-table/DataTable.vue'
 import { hostColumns } from './hostColumns.ts'
 import { nodeColumns } from './nodeColumns.ts'
-import {Alert, AlertDescription, AlertTitle} from "@components/ui/alert/index.js";
-import {toast} from "vue-sonner";
+import { Alert, AlertDescription, AlertTitle } from '@components/ui/alert/index.js'
+import { toast } from 'vue-sonner'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card/index.js'
+import Copyable from '@components/Copyable.vue'
 
 export default {
   components: {
-    AlertDescription, Alert, AlertTitle,
+    AlertDescription,
+    Alert,
+    AlertTitle,
     DataTable,
     Button,
     CardContent,
@@ -100,7 +113,12 @@ export default {
     TabsTrigger,
     PrincipalList,
     PermissionList,
-    EdgeContainer
+    EdgeContainer,
+    CardDescription,
+    CardFooter,
+    CardTitle,
+    CardHeader,
+    Copyable,
   },
 
   setup () {
@@ -124,7 +142,7 @@ export default {
       },
       deep: true,
       immediate: true,
-    }
+    },
   },
 
   mounted () {
@@ -164,9 +182,12 @@ export default {
       // - The hosts that are assigned to this cluster and their details (we get this from 747a62c9-1b66-4a2e-8dd9-0b70a91b6b75 Edge cluster status)
       // - The Nodes in this cluster, their name, status and\ other information (we get this from useNodeStore)
 
-      this.cluster.uuid = uuid;
+      this.cluster.uuid = uuid
 
-      const edgeClusterConfigResponse = await useServiceClientStore().client.ConfigDB.fetch(`/v1/app/${UUIDs.App.EdgeClusterConfiguration}/object/${this.cluster.uuid}`)
+      const edgeClusterConfigResponse = await useServiceClientStore().
+        client.
+        ConfigDB.
+        fetch(`/v1/app/${UUIDs.App.EdgeClusterConfiguration}/object/${this.cluster.uuid}`)
 
       const edgeClusterConfig = edgeClusterConfigResponse[1]
       this.cluster.name       = edgeClusterConfig.name

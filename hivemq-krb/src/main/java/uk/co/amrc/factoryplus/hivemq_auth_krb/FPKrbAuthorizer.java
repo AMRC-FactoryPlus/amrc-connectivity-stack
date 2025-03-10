9cb425af912d882de816175b53bf7f0eede01183
D J Newbould <D.J.Newbould@sheffield.ac.uk>
2025-03-10 08:30:30 +0000

    Fix async implemnation of authorizers
    The previous sync implementation could cause the broker the hang, using the async authorizers remediates this issue.

--- hivemq-krb/src/main/java/uk/co/amrc/factoryplus/hivemq_auth_krb/FPKrbAuthorizer.java
+++ hivemq-krb/src/main/java/uk/co/amrc/factoryplus/hivemq_auth_krb/FPKrbAuthorizer.java
@@ -6,14 +6,18 @@
 package uk.co.amrc.factoryplus.hivemq_auth_krb;
 
 import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extension.sdk.api.async.Async;
 import com.hivemq.extension.sdk.api.auth.PublishAuthorizer;
 import com.hivemq.extension.sdk.api.auth.SubscriptionAuthorizer;
 import com.hivemq.extension.sdk.api.auth.parameter.*;
+import com.hivemq.extension.sdk.api.services.ManagedExtensionExecutorService;
+import com.hivemq.extension.sdk.api.services.Services;
 import io.reactivex.rxjava3.core.Single;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.co.amrc.factoryplus.FPServiceClient;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,50 +33,70 @@ public class FPKrbAuthorizer implements SubscriptionAuthorizer, PublishAuthorize
 
     @Override
     public void authorizePublish(
-            @NotNull PublishAuthorizerInput publishAuthorizerInput,
-            @NotNull PublishAuthorizerOutput publishAuthorizerOutput
+            @NotNull PublishAuthorizerInput input,
+            @NotNull PublishAuthorizerOutput output
     ) {
-        var clientId = publishAuthorizerInput.getClientInformation().getClientId();
+        var clientId = input.getClientInformation().getClientId();
         var clientUsername = provider.context.getUsername(clientId);
-        var topic = publishAuthorizerInput.getPublishPacket().getTopic();
-        isPermissionAllowed(getACLforPrincipal(clientUsername, provider.fplus), topic, TopicPermission.MqttActivity.PUBLISH)
-                .subscribe(result -> {
-                    if(result){
-                        publishAuthorizerOutput.authorizeSuccessfully();
-                        log.info("Successfully authorized publish client {} for topic {}", clientUsername, topic);
-                    }else{
-                        log.info("Publish permission denied for user {} topic {}",clientUsername, topic);
-                        publishAuthorizerOutput.failAuthorization();
-                    }
-                },
-                        error ->  {
-                            log.error("Error occurred: {}", error.getMessage());
-                            publishAuthorizerOutput.failAuthorization();
-                        });
+        var topic = input.getPublishPacket().getTopic();
+        //get the managed extension executor service
+        final ManagedExtensionExecutorService extensionExecutorService = Services.extensionExecutorService();
+
+        //make the output async with a timeout of 2 seconds
+        final Async<PublishAuthorizerOutput> async = output.async(Duration.ofSeconds(2));
+
+        //submit a task to the extension executor
+        extensionExecutorService.submit(() -> {
+            isPermissionAllowed(getACLforPrincipal(clientUsername, provider.fplus), topic, TopicPermission.MqttActivity.PUBLISH)
+                    .subscribe(result -> {
+                                if(result){
+                                    output.authorizeSuccessfully();
+                                    log.info("Successfully authorized publish client {} for topic {}", clientUsername, topic);
+                                }else{
+                                    log.info("Publish permission denied for user {} topic {}",clientUsername, topic);
+                                    output.failAuthorization();
+                                }
+                                async.resume();
+                            },
+                            error ->  {
+                                log.error("Error occurred: {}", error.getMessage());
+                                output.failAuthorization();
+                                async.resume();
+                            });
+        });
     }
 
     @Override
     public void authorizeSubscribe(
-            @NotNull SubscriptionAuthorizerInput subscriptionAuthorizerInput,
-            @NotNull SubscriptionAuthorizerOutput subscriptionAuthorizerOutput
+            @NotNull SubscriptionAuthorizerInput input,
+            @NotNull SubscriptionAuthorizerOutput output
     ) {
-        var clientId = subscriptionAuthorizerInput.getClientInformation().getClientId();
+        var clientId = input.getClientInformation().getClientId();
         var clientUsername = provider.context.getUsername(clientId);
-        var topic = subscriptionAuthorizerInput.getSubscription().getTopicFilter();
-        isPermissionAllowed(getACLforPrincipal(clientUsername, provider.fplus), topic, TopicPermission.MqttActivity.SUBSCRIBE)
-                .subscribe(result -> {
-                    if (result) {
-                        log.info("Successfully authorized subscription client {} for topic {}.", clientUsername, topic);
-                        subscriptionAuthorizerOutput.authorizeSuccessfully();
-                    } else {
-                        log.info("Subscription permission denied for user {} topic {}", clientUsername, topic);
-                        subscriptionAuthorizerOutput.failAuthorization();
-                    }
-                },
-                        error ->  {
-                            log.error("Error occurred: {}", error.getMessage());
-                            subscriptionAuthorizerOutput.failAuthorization();
-                        });
+        var topic = input.getSubscription().getTopicFilter();
+        //get the managed extension executor service
+        final ManagedExtensionExecutorService extensionExecutorService = Services.extensionExecutorService();
+        //make the output async with a timeout of 2 seconds
+        final Async<SubscriptionAuthorizerOutput> async = output.async(Duration.ofSeconds(2));
+        //submit a task to the extension executor
+        extensionExecutorService.submit(() -> {
+            isPermissionAllowed(getACLforPrincipal(clientUsername, provider.fplus), topic, TopicPermission.MqttActivity.SUBSCRIBE)
+                    .subscribe(result -> {
+                                if (result) {
+                                    log.info("Successfully authorized subscription client {} for topic {}.", clientUsername, topic);
+                                    output.authorizeSuccessfully();
+                                } else {
+                                    log.info("Subscription permission denied for user {} topic {}", clientUsername, topic);
+                                    output.failAuthorization();
+                                }
+                                async.resume();
+                            },
+                            error ->  {
+                                log.error("Error occurred: {}", error.getMessage());
+                                output.failAuthorization();
+                                async.resume();
+                            });
+        });
     }
 
     /**

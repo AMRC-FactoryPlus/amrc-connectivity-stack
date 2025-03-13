288f91a6a427506c22e327b963d8e17a8545c564
Bradley Ramsay <b.r.ramsay@amrc.co.uk>
2025-03-13 09:15:20 +0000

    comments

--- acs-files/src/config/multer.js
+++ acs-files/src/config/multer.js
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const multer_storage = multer.diskStorage({
   destination: './uploads', // updated to the Persistent Volume in fpd-bender, which serves the pod file-service-test-pod
   //destination: '/mnt/disks/ssd1', // updated to the Persistent Volume in fpd-bender, which serves the pod file-service-test-pod
+  //put this location in an env variable
 
   filename: (req, file, cb) => {
     const file_uuid = uuidv4();

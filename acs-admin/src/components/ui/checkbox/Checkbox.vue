1e9929e32715ee0d3f054cac80d7c704ff5b7bf7
Kallum Burgin <Kallb123@users.noreply.github.com>
2025-03-14 12:02:06 +0000

    Update shad-cn init file to allow pulling components

--- acs-admin/src/components/ui/checkbox/Checkbox.vue
+++ acs-admin/src/components/ui/checkbox/Checkbox.vue
@@ -1,75 +1,38 @@
-<!--
-  - Copyright (c) University of Sheffield AMRC 2025.
-  -->
-
 <script setup>
-import { cn } from '@/lib/utils'
-import { Check } from 'lucide-vue-next'
-import { CheckboxIndicator, CheckboxRoot, useForwardPropsEmits } from 'reka-ui'
-import { computed } from 'vue'
+import { cn } from '@/lib/utils';
+import { Check } from 'lucide-vue-next';
+import { CheckboxIndicator, CheckboxRoot, useForwardPropsEmits } from 'reka-ui';
+import { computed } from 'vue';
 
 const props = defineProps({
-  defaultChecked: {
-    type: Boolean,
-    required: false,
-  },
-  checked: {
-    type: [Boolean, String],
-    required: false,
-  },
-  disabled: {
-    type: Boolean,
-    required: false,
-  },
-  required: {
-    type: Boolean,
-    required: false,
-  },
-  name: {
-    type: String,
-    required: false,
-  },
-  value: {
-    type: String,
-    required: false,
-  },
-  id: {
-    type: String,
-    required: false,
-  },
-  asChild: {
-    type: Boolean,
-    required: false,
-  },
-  as: {
-    type: null,
-    required: false,
-  },
-  class: {
-    type: null,
-    required: false,
-  },
-})
-const emits = defineEmits(['update:checked'])
+  defaultValue: { type: [Boolean, String], required: false },
+  modelValue: { type: [Boolean, String, null], required: false },
+  disabled: { type: Boolean, required: false },
+  value: { type: [String, Number, Object, null], required: false },
+  id: { type: String, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  name: { type: String, required: false },
+  required: { type: Boolean, required: false },
+  class: { type: null, required: false },
+});
+const emits = defineEmits(['update:modelValue']);
 
 const delegatedProps = computed(() => {
-  const {
-          class: _,
-          ...delegated
-        } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
 
-const forwarded = useForwardPropsEmits(delegatedProps, emits)
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
 </script>
 
 <template>
   <CheckboxRoot
-      v-bind="forwarded"
-      :class="
+    v-bind="forwarded"
+    :class="
       cn(
-        'peer flex items-center justify-center size-4 shrink-0 rounded-sm border border-gray-500 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=checked]:text-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900',
+        'peer h-4 w-4 shrink-0 rounded-sm border border-slate-200 border-slate-900 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=checked]:text-slate-50 dark:border-slate-800 dark:border-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900',
         props.class,
       )
     "

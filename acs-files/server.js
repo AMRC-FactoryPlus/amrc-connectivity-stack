cb5c9fc77b61f4e684f0d100db74b3c0f2e71c14
Bradley Ramsay <b.r.ramsay@amrc.co.uk>
2025-03-17 11:38:29 +0000

    remove old structure

--- acs-files/server.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import app from './src/app.js';
-
-dotenv.config();
-
-// Start server
-const PORT = process.env.PORT || 19834;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

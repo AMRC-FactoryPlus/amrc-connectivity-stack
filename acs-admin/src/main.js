7ea76ac201661f029029d7fac58bc808f2287223
Alex Godbehere <alex.godbehere@gmail.com>
2025-03-27 16:53:32 +0000

    Disable auth guard redirection in main.js to prevent login loop on page refresh

--- acs-admin/src/main.js
+++ acs-admin/src/main.js
@@ -91,18 +91,21 @@ const router = createRouter({
   routes,
 })
 
+// XXX - Disabled because it kept redirecting me to login every time I
+// refreshed the page.
+
 // Setup auth guard.
-router.beforeEach((to, from, next) => {
-  const s = useServiceClientStore();
-  if(!s.loaded && to.path !== "/login"){
-    next({path: "/login"})
-  }else if(s.loaded && to.path === "/login"){
-    next({path: "/"})
-  }
-  else{
-    next();
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   const s = useServiceClientStore();
+//   if(!s.loaded && to.path !== "/login"){
+//     next({path: "/login"})
+//   }else if(s.loaded && to.path === "/login"){
+//     next({path: "/"})
+//   }
+//   else{
+//     next();
+//   }
+// })
 
 const pinia = createPinia()
 

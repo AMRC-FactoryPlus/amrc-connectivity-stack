273663625764c18c36ea667691bdf6f4f1939736
Alex Godbehere <alex.godbehere@gmail.com>
2025-03-27 15:33:21 +0000

    Add additional info to useConnectionStore.js

--- acs-admin/src/store/useConnectionStore.js
+++ acs-admin/src/store/useConnectionStore.js
@@ -3,12 +3,18 @@
  */
 
 import { UUIDs } from "@amrc-factoryplus/service-client"
-import { useStore } from '@store/useStore.js'
+import { useStore } from '@store/useStore.ts'
 
 export const useConnectionStore = () => useStore(
-  'connection', 
-  UUIDs.Class.EdgeAgentConnection,
-  {
-    configuration: UUIDs.App.ConnectionConfiguration,
-  }
+    'connection',
+    UUIDs.Class.EdgeAgentConnection,
+    {
+      configuration: {
+        app: UUIDs.App.ConnectionConfiguration,
+        appBindings: {
+          driver: UUIDs.App.DriverDefinition,
+          'topology.cluster': UUIDs.App.EdgeClusterConfiguration,
+        }
+      }
+    }
 )()

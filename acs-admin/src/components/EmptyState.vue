b61843472476dc4018e70ac1e6192fb7ba81b792
Alex Godbehere <alex.godbehere@gmail.com>
2025-03-27 15:36:54 +0000

    Add icon support to EmptyState component

--- acs-admin/src/components/EmptyState.vue
+++ acs-admin/src/components/EmptyState.vue
@@ -4,7 +4,8 @@
 
 <template>
   <div class="flex flex-col items-center justify-center p-10 space-y-8 h-full max-w-xl mx-auto">
-    <div class="text-center flex flex-col gap-3">
+    <div class="text-center flex flex-col gap-2">
+      <i v-if="icon" :class="`fa-solid fa-${icon} fa-2x mb-3`"></i>
       <h3 class="text-xl font-semibold text-gray-900 dark:text-gray-50">{{ title }}</h3>
       <p class="text-gray-500 dark:text-gray-400">{{ description }}</p>
     </div>
@@ -39,6 +40,10 @@ export default {
       default: null
     },
     buttonIcon: {
+      type: String,
+      default: null
+    },
+    icon: {
       type: String,
       default: null
     }

0a91ef0e6378f649c4e992b32a8247a5ff0a42b3
Alex Godbehere <alex.godbehere@gmail.com>
2025-03-27 15:36:02 +0000

    Update import path for useStore in useDriverStore.js to TypeScript file

--- acs-admin/src/store/useDriverStore.js
+++ acs-admin/src/store/useDriverStore.js
@@ -3,7 +3,7 @@
  */
 
 import { UUIDs } from "@amrc-factoryplus/service-client"
-import { useStore } from '@store/useStore.js'
+import { useStore } from '@store/useStore.ts'
 
 export const useDriverStore = () => useStore(
   'driver', 

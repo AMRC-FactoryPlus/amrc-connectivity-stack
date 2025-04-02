bb44858744b7d306d91dd67725befad4ddd98074
D J Newbould <D.J.Newbould@sheffield.ac.uk>
2025-04-02 16:07:02 +0100

    Add edge agent migration task
    The edge agent config is split in to connection config and device information needed for the v4 manager.

---
+++ acs-service-setup/lib/manager-devices.js
@@ -0,0 +1,78 @@
+/*
+ * ACS service setup
+ * Migrate Manager edge agent config to devices and connections in the ConfigDB
+ * Copyright 2025 University of Sheffield AMRC
+ */
+
+import {UUIDs} from "@amrc-factoryplus/service-client";
+
+export async function migrate_edge_agent_config(ss) {
+    const {fplus} = ss;
+    const cdb = fplus.ConfigDB;
+    const log = fplus.debug.bound("manager");
+
+    const edgeAgentConfigUUIDs = await cdb.list_configs(UUIDs.App.EdgeAgentConfig);
+    const driverUUIDs = await cdb.list_configs(UUIDs.App.DriverDefinition);
+    const drivers = {};
+    log(JSON.stringify(driverUUIDs));
+    // Find and build driver name lookup.
+    for (const driverUUID of driverUUIDs){
+        const driverInfo = await cdb.get_config(UUIDs.App.Info, driverUUID);
+        drivers[driverInfo.name] = driverUUID;
+    }
+
+    for (const uuid of edgeAgentConfigUUIDs) {
+        const edgeAgentConfig = await cdb.get_config(UUIDs.App.EdgeAgentConfig, uuid);
+        const edgeDeploymentConfig = await cdb.get_config(UUIDs.App.EdgeAgentDeployment, uuid);
+        // TODO: add a check to skip config if it already exists in the new apps
+        for (const connection of edgeAgentConfig.deviceConnections) {
+            const connectionObjectUUID = await cdb.create_object(UUIDs.Class.EdgeAgentConnection);
+            await cdb.put_config(UUIDs.App.Info, connectionObjectUUID, {
+                name: connection.name
+            });
+            // Remove any special characters.
+            const connectionConfigKey = (connection.connType).replace(/[^a-zA-Z0-9]/g, '');
+            // Insert into connection config
+            const connectionConfig = {
+                config: connection[(connectionConfigKey + "ConnDetails")],
+                createdAt: new Date().toISOString(),
+                deployment: {},
+                driver: drivers[connectionConfigKey],
+                edgeAgent: uuid,
+                source: {
+                    payloadFormat: connection.payloadFormat
+                },
+                topology: {
+                    cluster: edgeDeploymentConfig.cluster,
+                    host: edgeDeploymentConfig.hostname,
+                    node: uuid,
+                }
+            }
+            await cdb.put_config(UUIDs.App.ConnectionConfiguration, connectionObjectUUID, connectionConfig);
+            // Migrate device information
+            for (const device of connection.devices){
+                // create object
+                const deviceObjectUUID = await cdb.create_object(UUIDs.Class.Device);
+                // create object information
+                await cdb.put_config(UUIDs.App.Info, deviceObjectUUID, {
+                    name: device.deviceId
+                });
+                // Find the schema uuid tag.
+                const schemaTag = device.tags.find(t => t.Name === "Value/Schema_UUID");
+
+                const deviceInformationPayload = {
+                    connection: connectionObjectUUID,
+                    createdAt: new Date().toISOString(),
+                    node: uuid,
+                    originMap: device.tags,
+                    schema: schemaTag.value,
+                    sparkplugName: device.deviceId,
+                };
+                // Insert the config.
+                await cdb.put_config(UUIDs.App.DeviceInformation, deviceObjectUUID, deviceInformationPayload);
+            }
+
+        }
+    }
+}
+

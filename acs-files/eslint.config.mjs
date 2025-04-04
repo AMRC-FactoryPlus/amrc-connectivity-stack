24faa4c80fecbecc69fa9cfcba380e3c9c448bfd
D J Newbould <D.J.Newbould@sheffield.ac.uk>
2025-04-04 14:29:49 +0100

    Add eslint

---
+++ acs-files/eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+    js.configs.recommended,
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+
+        rules: {
+            "no-unreachable": "warn",
+            "no-unused-vars": ["warn", {
+                args:           "none",
+                caughtErrors:   "none",
+            }],
+                
+        },
+    },
+];

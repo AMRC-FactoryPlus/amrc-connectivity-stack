a902ad4a4e24003024b6ec55673f5be13d1cdf1e
Alex Godbehere <alex.godbehere@gmail.com>
2025-04-10 14:03:03 +0100

    fix: add padding to the layout in EdgePageSkeleton component

--- acs-admin/src/components/EdgeManager/EdgePageSkeleton.vue
+++ acs-admin/src/components/EdgeManager/EdgePageSkeleton.vue
@@ -3,7 +3,7 @@
   -->
 
 <template>
-  <div class="flex flex-col gap-4 justify-center">
+  <div class="flex flex-col gap-4 justify-center p-4">
     <div class="w-full h-14 rounded-md bg-gray-200 animate-pulse"></div>
     <div class="w-full h-32 rounded-md bg-gray-200 animate-pulse"></div>
     <div class="w-48 h-10 rounded-md bg-gray-200 animate-pulse"></div>

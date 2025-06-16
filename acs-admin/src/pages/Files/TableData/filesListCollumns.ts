e4ec45535caef5b077787ed82f2dd1b36333563d
D J Newbould <d.j.newbould@sheffield.ac.uk>
2025-06-16 15:46:38 +0100

    Fix size sorting

--- acs-admin/src/pages/Files/TableData/filesListCollumns.ts
+++ acs-admin/src/pages/Files/TableData/filesListCollumns.ts
@@ -24,22 +24,27 @@ export interface ApplicationMapping {
 
 export const columns: ColumnDef<ApplicationMapping>[] = [
     {
-    accessorKey: 'name',
-    accessorFn: (row) => row?.filesConfiguration?.original_file_name ?? row.name,
-    header: ({column}) => h(DataTableColumnHeader, {
-        column,
-        title: 'Name'
-    }),
-    cell: ({row}) => {
-        return h('div', {class: 'max-w-[500px] truncate'}, [
-            h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('name')),
-            h('div', {class: 'max-w-[500px] truncate text-gray-400'}, row.original.uuid ?? "UNKNOWN")
-            ])
-        },
+        accessorKey: 'name',
+        accessorFn: (row) => row?.filesConfiguration?.original_file_name ?? row.name,
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Name'
+        }),
+        cell: ({row}) => {
+            return h('div', {class: 'max-w-[500px] truncate'}, [
+                h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('name')),
+                h('div', {class: 'max-w-[500px] truncate text-gray-400'}, row.original.uuid ?? "UNKNOWN")
+                ])
+            },
     },
     {
         accessorKey: 'size',
         accessorFn: (row) => row?.filesConfiguration?.file_size ? formatFileSize(row.filesConfiguration.file_size) : "--",
+        sortingFn: (rowA, rowB, columnId) => {
+            const sizeA = rowA.original.filesConfiguration?.file_size ?? 0;
+            const sizeB = rowB.original.filesConfiguration?.file_size ?? 0;
+            return sizeA - sizeB;
+        },
         header: ({column}) => h(DataTableColumnHeader, {
             column,
             title: 'Size'
@@ -51,32 +56,32 @@ export const columns: ColumnDef<ApplicationMapping>[] = [
         },
     },
     {
-    accessorKey: 'created',
-    accessorFn: (row) => row?.filesConfiguration?.date_uploaded ?
-        new Date(row.filesConfiguration.date_uploaded).toLocaleDateString("en-GB", {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false
-        }) :
-        "--",
-    sortingFn: (rowA, rowB, columnId) => {
-        const dateA = new Date(rowA.original.filesConfiguration?.date_uploaded ?? 0);
-        const dateB = new Date(rowB.original.filesConfiguration?.date_uploaded ?? 0);
-        return dateA.getTime() - dateB.getTime();
-    },
-    header: ({column}) => h(DataTableColumnHeader, {
-        column,
-        title: 'Created'
-    }),
-    cell: ({row}) => {
-        return h('div', {class: 'max-w-[500px] truncate'}, [
-            h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('created')),
-            ])
-        },
+        accessorKey: 'created',
+        accessorFn: (row) => row?.filesConfiguration?.date_uploaded ?
+            new Date(row.filesConfiguration.date_uploaded).toLocaleDateString("en-GB", {
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: false
+            }) :
+            "--",
+        sortingFn: (rowA, rowB, columnId) => {
+            const dateA = new Date(rowA.original.filesConfiguration?.date_uploaded ?? 0);
+            const dateB = new Date(rowB.original.filesConfiguration?.date_uploaded ?? 0);
+            return dateA.getTime() - dateB.getTime();
+            },
+        header: ({column}) => h(DataTableColumnHeader, {
+            column,
+            title: 'Created'
+        }),
+        cell: ({row}) => {
+            return h('div', {class: 'max-w-[500px] truncate'}, [
+                h('div', {class: 'max-w-[500px] truncate font-medium'}, row.getValue('created')),
+                ])
+            },
     },
     {
         accessorKey: 'createdBy',
@@ -92,7 +97,7 @@ export const columns: ColumnDef<ApplicationMapping>[] = [
         },
     },
     {
-    id: 'actions',
+        id: 'actions',
         header: ({column}) => h(DataTableColumnHeader, {
             column,
             title: ''

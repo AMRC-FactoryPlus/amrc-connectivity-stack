97556b888c91457f2bc800c9d285ac102342b408
Kavan Price <kavanprice@gmail.com>
2025-09-23 17:24:36 +0100

    Switch to AsyncDriver
    This makes more sense given the behaviour of the underlying library.

--- edge-rtde/bin/driver.js
+++ edge-rtde/bin/driver.js
@@ -2,13 +2,12 @@
  * Copyright (c) University of Sheffield AMRC 2025.
  */
 
-import { PolledDriver } from "@amrc-factoryplus/edge-driver";
+import { AsyncDriver } from "@amrc-factoryplus/edge-driver";
 import { RTDEHandler } from "../lib/rtde.js";
 
-const drv = new PolledDriver({
+const drv = new AsyncDriver({
   env: process.env,
-  handler: RTDEHandler,
-  serial: true,
+  handler: RTDEHandler
 });
 
 drv.run();
--- edge-rtde/lib/rtde.js
+++ edge-rtde/lib/rtde.js
@@ -11,15 +11,15 @@ export class RTDEHandler {
     this.driver = driver;
     this.conf = conf;
     this.log = driver.debug.bound("rtde");
-    this.client = null;
-    //this.samples = conf.samples || 5;
-    this.polling = conf.polling || 1000; // Default polling interval (ms)
     this.lastPoll = 0;
     this.tmp = 0;
-    this.on_close = () => {
-      this.log("RTDE connection closed");
-      driver.connFailed();
-    };
+
+    // Set up the TCP socket for the southbound RTDE device
+    this.client = new net.Socket();
+    this.client.on("error", (err) => {
+      this.log("RTDE client error: %o", err)
+      this.driver.connFailed();
+    });
   }
 
   static create(driver, conf) {
@@ -30,54 +30,36 @@ export class RTDEHandler {
 
   connect() {
     const { host, port } = this.conf;
-    this.client = new net.Socket();
-    this.client.on("close", this.on_close);
-
-    return new Promise((resolve, reject) => {
-      this.client.connect(port, host, () => {
-        this.log(`Connected to RTDE on ${host}:${port}`);
-        this.driver.connUp();
-        resolve("UP");
-      });
-
-      this.client.on("data", (data) => {
-        this.poll(data);
-      });
-
-      this.client.on("error", (err) => {
-        this.log("RTDE client error: %o", err);
-        this.driver.connFailed();
-        reject("CONN");
-      });
+    this.client.connect(port, host, () => {
+      this.log(`Connected to RTDE on ${host}:${port}`);
+      this.driver.connUp();
     });
   }
 
-  close(done) {
-    if (this.client) {
-      this.client.off("close", this.on_close);
-      this.client.destroy();
-      this.client = null;
-    }
-    if (done) done();
+  subscribe(_specs) {
+    // The library requires us to subscribe to all data on the device
+    this.client.on("data", (data) => {
+      // This constructs a new urState object
+      const state = new ur().onData(data);
+      if (state !== undefined) {
+        this.handleData(state);
+      }
+    });
   }
 
-  parseAddr(addr) {
-    
+  async close() {
+    return new Promise((resolve) => {
+      this.client.once("close", () => {
+        this.log("RTDE connection closed.");
+        resolve();
+      });
+
+      this.client.end();
+    });
   }
 
-  poll(data) {
-    // Polling logic: only forward data at the configured interval
-        // Parse RTDE data using ur.js
-    const res = new ur().onData(data);
-    if (res !== undefined) {
-      //this.log("RTDE data: %s", JSON.stringify(res));
-      // Forward parsed data to driver if needed
-      if (this.driver.data) this.driver.data(res);
-    }
-    //this.tmp++;
-    //if (this.tmp >= this.samples) {
-    //  this.client.destroy();
-    //}
+  handleData(urState) {
+    // TODO: handle urState data
   }
 }
 

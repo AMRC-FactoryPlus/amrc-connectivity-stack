c3bbb1c8cbc176b05cf67dd050bda91725794a51
Ben Morrow <b.morrow@amrc.co.uk>
2025-11-27 11:00:08 +0000

    Grant Git permission to upload schemas
    Update our config schema.

--- acs-service-setup/lib/uuids.js
+++ acs-service-setup/lib/uuids.js
@@ -7,9 +7,10 @@ export const ACS = {
         EdgeDeployment:     "e6f6a6e6-f6b2-422a-bc86-2dcb417a362a",
     },
     App: {
+        JsonSchema:         "b16e85fb-53c2-49f9-8d83-cdf6763304ba",
+        SchemaMetadata:     "32093857-9d29-470e-a897-d2b56d5aa978",
         SchemaIcon:         "65c0ccba-151d-48d3-97b4-d0026a811900",
         MQTTPermTemplate:   "1266ddf1-156c-4266-9808-d6949418b185",
-
     },
     /* XXX These might really be Applications? For now they are
      * well-known to the Auth service; they are perhaps 'classes of

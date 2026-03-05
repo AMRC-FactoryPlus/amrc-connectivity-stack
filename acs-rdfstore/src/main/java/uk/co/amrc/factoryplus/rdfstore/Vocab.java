c0e6fad443fad9c5f08d2c8ded9824358575fd09
Ben Morrow <b.morrow@amrc.co.uk>
2026-03-05 13:18:01 +0000

    Publish both direct and derived graphs
    Supply the API with a Dataset holding both direct and derived graphs,
    under F+ IRIs. Also install the derived graph as the default graph. The
    ConfigDB /direct/ endpoints will need access to the direct graph.

---
+++ acs-rdfstore/src/main/java/uk/co/amrc/factoryplus/rdfstore/Vocab.java
@@ -0,0 +1,52 @@
+/*
+ * Factory+ RDF store
+ * General utilities
+ * Copyright 2026 University of Sheffield AMRC
+ */
+
+package uk.co.amrc.factoryplus.rdfstore;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.net.URI;
+
+import io.vavr.control.Try;
+
+import org.apache.jena.rdf.model.*;
+
+public class Vocab
+{
+    public static final String NS = "http://factoryplus.app.amrc.co.uk/rdf/";
+    public static final String NS_uuid      = NS + "uuid/";
+    public static final String NS_core      = NS + "core/";
+    public static final String NS_graph     = NS + "graph/";
+
+    private static Property coreP (String p) {
+        return ResourceFactory.createProperty(NS_core + p);
+    }
+
+    public static final Property uuid       = coreP("uuid");
+
+    private static Resource graph (String g) {
+        return ResourceFactory.createResource(NS_graph + g);
+    }
+
+    public static final Resource G_direct   = graph("direct");
+    public static final Resource G_derived  = graph("derived");
+
+    public static Resource fromUuid (UUID uuid)
+    {
+        return ResourceFactory.createResource(
+            NS_uuid + uuid.toString());
+    }
+
+    public static Optional<UUID> uuidFromUri (String uri)
+    {
+        return Optional.of(uri)
+            .filter(u -> u.startsWith(NS_uuid))
+            .map(u -> u.substring(NS_uuid.length()))
+            .flatMap(u -> Try.success(u)
+                .mapTry(UUID::fromString)
+                .toJavaOptional());
+    }
+}

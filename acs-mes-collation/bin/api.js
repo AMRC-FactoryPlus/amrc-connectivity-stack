75879ba2cf3f0a809f2d66aa56c40fa3e8ce4361
Kavan Price <kavanprice@gmail.com>
2026-03-19 10:52:09 +0000

    Wire up to deploy

--- acs-mes-collation/bin/api.js
+++ acs-mes-collation/bin/api.js
@@ -5,8 +5,8 @@
  * Application entrypoint
  */
 
-import { RxClient }         from "@amrc-factoryplus/rx-client";
-import { WebAPI }           from "@amrc-factoryplus/service-api";
+import { RxClient } from "@amrc-factoryplus/rx-client";
+import { WebAPI } from "@amrc-factoryplus/service-api";
 
 import { GIT_VERSION } from "../lib/git-version.js";
 import { Service, Version } from "../lib/constants.js";
@@ -24,27 +24,29 @@ const model = await new Model({ auth, fplus }).init();
 
 const api = await new WebAPI({
     ping: {
-        version:    Version,
-        service:    Service.MESCollation,
-        device:     env.DEVICE_UUID,
+        version: Version,
+        service: Service.MESCollation,
+        device: env.DEVICE_UUID,
         software: {
-            vendor:         "AMRC",
-            application:    "acs-mes-collation",
-            revision:       GIT_VERSION,
+            vendor: "AMRC",
+            application: "acs-mes-collation",
+            revision: GIT_VERSION,
         },
     },
-    debug:      fplus.debug,
-    realm:      env.REALM,
-    hostname:   env.HOSTNAME,
-    keytab:     env.SERVER_KEYTAB,
-    http_port:  env.PORT,
-    max_age:    env.CACHE_MAX_AGE,
+    debug: fplus.debug,
+    realm: env.REALM,
+    hostname: env.HOSTNAME,
+    keytab: env.SERVER_KEYTAB,
+    http_port: env.PORT,
+    max_age: env.CACHE_MAX_AGE,
 
-    routes:     routes({ auth, model, fplus }),
+    routes: routes({ auth, model, fplus }),
 }).init();
 
 const notify = new MESNotify({
-    auth, api, model,
+    auth,
+    api,
+    model,
     debug: fplus.debug,
 });
 

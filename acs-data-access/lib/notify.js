125cd46d3aa7b329df5b28f66d1693d3c7208b81
amrc-za <z.abduraimova@sheffield.ac.uk>
2026-03-20 15:40:21 +0000

    data access skeleton

---
+++ acs-data-access/lib/notify.js
@@ -0,0 +1,79 @@
+/*
+* ACS Data Access Service
+* DataAccessNotify
+*/
+
+
+import * as rx from "rxjs";
+
+import * as rxx from "@amrc-factoryplus/rx-util";
+import { Notify } from "@amrc-factoryplus/service-api";
+
+export class DataAccessNotify {
+  constructor(opts) {
+    this.data = opts.data;
+    this.log = opts.debug.bound("notify");
+
+    this.notify = this.build_notify(opts.api);
+  }
+
+  run() {
+    this.log("Running Data Access Notify Server");
+    this.notify.run();
+  }
+
+  /**
+   * constructs a Notify object 
+   * acts as an express router
+   * routes notify requests to methods you give it.
+   */
+  build_notify(api) {
+    const notify = new Notify({
+      api,
+      log: this.log,
+    });
+
+    // This is basically Express. This is more strict in that directories must end with a "/". It won't redirect.
+
+    notify.watch("v1/metadata", this.metadata_list.bind(this));
+    notify.watch("v1/metadata/:uuid", )
+    notify.search("v1/metadata/", )
+    
+    notify.watch("v1/structure/", )
+    notify.watch("v1/structure/:uuid", )
+    notify.search("v1/structure/", )
+    
+    return notify;
+  }
+
+  /**
+   * Returns a list of Dataset UUIDs the client has access to
+   */
+  metadata_list(sess){
+    
+  }
+
+  /**
+  * Get names for all objects the principal can see.
+  */
+  name_list(sess) {
+    // We should combine this with the session permissions to see the objects
+    return rxx.rx(
+      this.data.names,
+      rx.map(entries => entries.keySeq().toArray()),
+      rx.mergeMap(
+        // You need to filter out what they can't see here using `sess` (not in a .filter :))
+      ),
+      rx.map(
+        // You need to build your response objects here
+      ));
+  }
+
+
+  /**
+  * Get changes to the name of a particular object `obj`.
+  */
+  object_name(sess, obj) {
+    // Do similar to `name_list` here.
+  }
+}

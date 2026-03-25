da5b94e27691132296c9b92a371b9b18f92ab4db
amrc-za <z.abduraimova@sheffield.ac.uk>
2026-03-25 12:15:30 +0000

    data access - dataflow build metadata and code clean up

--- acs-data-access/bin/api.js
+++ acs-data-access/bin/api.js
@@ -8,9 +8,9 @@
 
 import { RxClient, UUIDs } from '@amrc-factoryplus/rx-client';
 import { WebAPI } from '@amrc-factoryplus/service-api';
-import { routes } from '../lib/routes.js';
-// import { Version, Service } from '../lib/constants.js';
 import {DataFlow} from '../lib/dataflow.js';
+import { APIv1 } from '../lib/api-v1.js';
+import {DataAccessNotify} from '../lib/notify.js';
 
 const { env } = process;
 
@@ -18,35 +18,52 @@ const fplus = await new RxClient({
   env,
 }).init();
 
+const Version = "2.0.0";
 
 const debug = fplus.debug;
 debug.log("app", "Starting acs-data-service revision %s");
+
 
 // the dataflow object is full of sequences that does most of the work of this service. Sequence means an RX observable.
 const data = new DataFlow({
   fplus
 });
 
-// const api = await new WebAPI({
-//   ping: {
-//     version: Version,
-//     service: UUIDs.Service.DataAccess,
-//     software: {
-//       vendor: 'AMRC',
-//       application: 'acs-data-access',
-//     },
-//   },
-//   debug: fplus.debug,
-//   realm: env.REALM,
-//   hostname: env.HOSTNAME,
-//   keytab: env.SERVER_KEYTAB,
-//   http_port: env.PORT,
-//   max_age: env.CACHE_MAX_AGE,
-//   routes: routes({
-//     fplus,
-//   }),
-// }).init();
 
-// api.run();
+const apiv1 = new APIv1({ data, debug });
 
+const api = await new WebAPI({
+  debug: fplus.debug,
+  ping: {
+    version: Version,
+    service: UUIDs.Service.DataAccess,
+    software: {
+      vendor: 'AMRC',
+      application: 'acs-data-access',
+    },
+  },
+  realm: env.REALM,
+  hostname: env.HOSTNAME,
+  keytab: env.SERVER_KEYTAB,
+  http_port: env.PORT,
+  max_age: env.CACHE_MAX_AGE,
+  routes: app => {
+    app.use("/v1", apiv1.routes);
+  }
+}).init();
+
+
+const notify = new DataAccessNotify({
+  api, data, debug
+});
+
+debug.log("app", "Running Data Access DataFlow");
 data.run();
+
+// debug.log("app", "Running Data Access Notify");
+// notify.run();
+
+debug.log("app", "Running Data Access WebAPI")
+api.run();
+
+
--- acs-data-access/lib/dataflow.js
+++ acs-data-access/lib/dataflow.js
@@ -2,14 +2,16 @@
 * ACS Data Access Service
 * Data-Flow / sequence management
 */
-import rx from 'rxjs';
 
+import imm from "immutable";
+import rx from "rxjs";
+
+// import { UUIDs }    from "@amrc-factoryplus/service-client";
 import * as rxx     from "@amrc-factoryplus/rx-util";
-import { RxClient, UUIDs } from '@amrc-factoryplus/rx-client';
+import { Optional, Response } from "@amrc-factoryplus/rx-util";
 
-import { DataAccess } from './constants.js';
-
-
+import { DataAccess as Constants }  from './constants.js';
+import { valid_uuid, valid_krb }    from "./validate.js";
 
 
 export class DataFlow {
@@ -20,104 +22,48 @@ export class DataFlow {
     this.cdb = fplus.ConfigDB;
     this.auth = fplus.Auth;
 
-    /* Requests to update the database */
-    /* it's a subject which means we can push values down the sequence. That is driven from requests to the api to make changes.
-    All the rest are driven from external sources of data (db or ConfigDB).  
-    */ 
-   /**
-    * for the DataFlow service we'd want to think about what sources of
-    * information we need to track from the configDB and we start building sequences that track that information and transform it into forms that are useful to us. 
-    */
-    this.requests = new rx.Subject(); 
-    /* Responses to these requests */
-    this.responses = this._build_responses();
     this.metadata = this._build_metadata();
-
-    // combine these into map ??? with key = structural dataset class UUID
-    this.sessions = this.cdb.search_app(DataAccess.App.SessionLimits);
-    this.unions = this.cdb.search_app(DataAccess.App.UnionComponents);
-
-
-    // this.names = this._build_names();
   }
 
-
-  _build_metadata(){
-
-  }
-
-  update_name(r) {
-    // Something like this but returning an HTTP-like response. (put_config returns an exception that needs catching)
-    this.cdb.put_config(UUIDs.App.Info, r.object, { name: r.name });
-  };
-
-  
-  _build_names() {
+  _build_metadata() {
     return rxx.rx(
-      this.cdb.search_app(UUIDs.App.Info),
-      // rx.map acts on the sequence of responses, entries.map acts on the entries maps
-      rx.map(entries => entries.map(entry => entry.name)),
-      // Cache the map (1 refers to 1 previous value)
-      rx.shareReplay(1));
-  }
+      rx.combineLatest([
+          this.cdb.search_app(Constants.App.SessionLimits),
+          this.cdb.search_app(Constants.App.UnionComponents)
+      ]),
+      rx.map(([sessions, unions]) => {
+        const sessionDatasets = sessions.keySeq().toList();
 
+        const unionDatasets = unions
+          .valueSeq()     // all arrays
+          .flatten()      // flatten arrays
+          .toSet()        // dedupe
+          .toList();
 
-
-  _build_responses() {
-    // this obj is a map from request type to a method that handles this request. 
-    const updaters = {
-      // This tells you what to do with a "name" type object
-      // UUIDs.App.Info == "General Information" application
-      name: this.update_name,
-    };
-
-    /**
-     * the processing loop that actually does the work which takes the sequence of requests that are coming in from the method down below where it pushes to the subject. And then each time we get a new request it looks up how we perform this request, it actually performs it with updater. 
-     * If we get a request that we don't have an updater for -> just return 500. If exists -> we call the updater.
-     */
-    return rxx.rx(this.requests,
-      rx.mergeMap(r => {
-        const updater = updaters[r.type]
-          ?? (async () => ({ status: 500 }));
-
-        return updater.call(this, r)
-          .then(rv => ({ ...rv, type: r.type, request: r.request }));
+        return imm.Map({
+          [Constants.Class.Session]: sessionDatasets,
+          [Constants.Class.Union]: unionDatasets,
+        });
       }),
-      rx.share());
+
+      rxx.shareLatest()
+    );
   }
-                                                                                                                                             
-  /**
-   * This function tells the data flow to start subscribing to things and making things happen.  
-   */
+
+
   run() {
-    this.sessions.subscribe(ss => this.log("SESSION UPDATE %o", ss.toJS()));
-    this.unions.subscribe(ss => this.log("UNION UPDATE %o", ss.toJS()));
-
+    this.metadata.subscribe(ss => this.log("METADATA UPDATE %o", ss.toJS()));
+    
   }
 
 
+  // Track list of datasets for principal with permission
+  track_metadata(principal, permission){
+    
+  }
 
 
+  find_metadata(principal){
 
-  /** Request a change to the database.
-   *
-   * This submits a change request to the database and returns a
-   * Promise containing an HTTP status code for the outcome. If a
-   * change is made then the change will also be published to
-   * `updates`.
-   *
-   * @param r A request object. Must contain a `kind` field.
-   * @returns A response object containing a `status` field.
-   */
-  // The actual processing of the request happens in the _build_responses method that builds the sequence. 
-  request(r) {
-    const request = uuidv4();
-    /* Construct the Promise to the response before we send the
-     * request. This avoids a race condition. */
-    const response = rx.firstValueFrom(rxx.rx(
-      this.responses,
-      rx.filter(r => r.request == request)));
-    this.requests.next({ ...r, request });
-    return response;
   }
 }

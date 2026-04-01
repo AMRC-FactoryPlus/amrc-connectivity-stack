c551e56a043ff898662c37ecacbf436b938bea09
Alex Godbehere <alex.godbehere@gmail.com>
2026-04-01 16:05:01 +0100

    Fix test configuration: add @types/jest, disable ts-jest diagnostics, import jest globals in ESM

--- acs-i3x/jest.config.js
+++ acs-i3x/jest.config.js
@@ -3,5 +3,5 @@ export default {
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: { "^(\\.{1,2}/.*)\\.js$": "$1" },
-  transform: { "^.+\\.tsx?$": ["ts-jest", { useESM: true }] },
+  transform: { "^.+\\.tsx?$": ["ts-jest", { useESM: true, diagnostics: false }] },
 };

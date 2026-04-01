a3d6e36369fbcb6990dbd7b5977c0b3c8fb586e2
Alex Godbehere <alex.godbehere@gmail.com>
2026-04-01 15:57:22 +0100

    Add i3X type definitions and constants

---
+++ acs-i3x/src/constants.ts
@@ -0,0 +1,13 @@
+export const Version = "0.1.0";
+export const I3X_SPEC_VERSION = "0.1.0";
+
+export const HIERARCHY_SCHEMA_UUID = "84ac3397-f3a2-440a-99e5-5bb9f6a75091";
+export const DEVICE_INFO_SCHEMA_UUID = "2dd093e9-1450-44c5-be8c-c0d78e48219b";
+
+// Built-in i3X relationship type IDs (synthetic, internal to this service)
+export const RelType = {
+    HasParent:     "i3x:rel:has-parent",
+    HasChildren:   "i3x:rel:has-children",
+    HasComponent:  "i3x:rel:has-component",
+    ComponentOf:   "i3x:rel:component-of",
+};

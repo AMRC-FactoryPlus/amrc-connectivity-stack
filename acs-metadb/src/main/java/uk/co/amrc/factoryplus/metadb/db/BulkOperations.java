e5f7d0eee3a82dc14934338be777d67301bd4ac9
Ben Morrow <b.morrow@amrc.co.uk>
2026-04-27 15:01:45 +0100

    Load object structure from dumps

--- acs-metadb/src/main/java/uk/co/amrc/factoryplus/metadb/db/BulkOperations.java
+++ acs-metadb/src/main/java/uk/co/amrc/factoryplus/metadb/db/BulkOperations.java
@@ -97,6 +97,26 @@ public class BulkOperations extends RequestHandler.Component
             ((JsonObject)obj).entrySet());
     }
 
+    /* Note here that <core/rank> only applies to the rank classes, and
+     * the value is the numerical rank of members of that rank class. So
+     * we have <core/Individual> <core/rank> 0. for instance, even
+     * though Individual itself is rank 1. We do not currently attempt
+     * to infer <core/rank> for other objects. */
+    private static final UpdateRequest U_setRankSuperclasses = Vocab.update("""
+        insert { ?obj rdfs:subClassOf ?class }
+        where {
+            ?obj <core/uuid> ?objU;
+                rdf:type/<core/rank> ?rank.
+
+            ?class <core/rank> ?crank.
+            filter (?crank = (?rank - 1))
+
+            filter not exists {
+                graph <graph/direct> {
+                    ?obj rdfs:subClassOf ?super.
+        } } }
+    """);
+
     public void loadDump (JsonValue jval)
     {
         if (!db().schemaTracker().validateDump(jval))
@@ -111,10 +131,61 @@ public class BulkOperations extends RequestHandler.Component
                         e1.getKey(), e2.getKey(), e2.getValue())))
                 .toList())
             .peek(this::loadObjects);
+
+        db().derived().rebind();
+        db().runUpdate(U_setRankSuperclasses);
+
+        db().validateZFC();
     }
 
     private void loadObjects (List<ObjSpec> objs)
     {
         log.info("Decoded obj specs: {}", objs);
+
+        var model = db().direct();
+
+        /* Create all objects first. */
+        for (var o: objs) {
+            /* This will cause a validation error if we have duplicate
+             * IRIs or UUIDs. This means it isn't possible to change the
+             * IRI-UUID mapping from a dump. This could be changed in
+             * future but it's not simple to see what to do. */
+            model.add(o.iri(), Vocab.uuid, Vocab.uuidLiteral(o.uuid()));
+            model.removeAll(o.iri(), Vocab.deleted, null);
+            model.removeAll(o.iri(), Vocab.name, null);
+            o.name().peek(n -> model.add(o.iri(), Vocab.name, n));
+        }
+        /* Now, if we fail to find a UUID, it is an error. */
+        for (var o: objs) {
+            var priR = db().findObjectOrError(o.primary());
+            model.removeAll(o.iri(), Vocab.primary, null);
+            model.add(o.iri(), Vocab.primary, priR);
+
+            model.removeAll(o.iri(), RDF.type, null);
+            o.memberOf()
+                .map(cs -> cs.map(db()::findObjectOrError))
+                .getOrElse(() -> List.of(priR))
+                .forEach(c -> model.add(o.iri(), RDF.type, c));
+
+            model.removeAll(o.iri(), RDFS.subClassOf, null);
+            o.subclassOf()
+                .map(cs -> cs.map(db()::findObjectOrError))
+                /* We must set the default rank subclasses later, after
+                 * we've rebound the derived graph. Otherwise we can't
+                 * find the ranks. */
+                .getOrElse(() -> List.empty())
+                .forEach(c -> model.add(o.iri(), RDFS.subClassOf, c));
+
+            o.enumMembers().peek(ms -> {
+                model.removeAll(null, RDF.type, o.iri());
+                ms.map(db()::findObjectOrError)
+                    .forEach(m -> model.add(m, RDF.type, o.iri()));
+            });
+
+            model.removeAll(o.iri(), Vocab.powersetOf, null);
+            o.powersetOf()
+                .map(db()::findObjectOrError)
+                .peek(set -> model.add(o.iri(), Vocab.powersetOf, set));
+        }
     }
 }

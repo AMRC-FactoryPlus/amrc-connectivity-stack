1cc028e396d7675b6759e9c1e6396a7a67d9789c
Ben Morrow <b.morrow@amrc.co.uk>
2026-04-27 12:22:32 +0100

    Start decoding object spec from dumps

--- acs-metadb/src/main/java/uk/co/amrc/factoryplus/metadb/db/BulkOperations.java
+++ acs-metadb/src/main/java/uk/co/amrc/factoryplus/metadb/db/BulkOperations.java
@@ -15,7 +15,7 @@ import org.apache.jena.rdf.model.*;
 import org.apache.jena.update.*;
 import org.apache.jena.vocabulary.*;
 
-import io.vavr.collection.Iterator;
+import io.vavr.collection.*;
 import io.vavr.control.Option;
 
 import org.slf4j.Logger;
@@ -30,6 +30,56 @@ public class BulkOperations extends RequestHandler.Component
 {
     private static final Logger log = LoggerFactory.getLogger(BulkOperations.class);
 
+    record ObjSpec (
+        UUID uuid,
+        Resource iri,
+        UUID primary,
+        Option<String> name,
+        Option<List<UUID>> memberOf,
+        Option<List<UUID>> subclassOf,
+        Option<UUID> powersetOf,
+        Option<List<UUID>> enumMembers)
+    {
+        private static String jsonString (JsonValue v)
+        {
+            return ((JsonString)v).getString();
+        }
+
+        private static Option<List<UUID>> maybeUuidList (JsonObject spec, String key)
+        {
+            return Option.of(spec.get(key))
+                .map(v -> Iterator.ofAll((JsonArray)v)
+                    .map(ObjSpec::jsonString)
+                    .map(Decoders::parseUUIDOrError)
+                    .toList());
+        }
+
+        public static ObjSpec ofJson (String pri, String obj, JsonValue jval)
+        {
+            var uuid = Decoders.parseUUIDOrError(obj);
+            var primary = Decoders.parseUUIDOrError(pri);
+            var spec = (JsonObject)jval;
+
+            var iri = Option.of(spec.get("iri"))
+                .map(ObjSpec::jsonString)
+                .map(Vocab::res)
+                .getOrElse(() -> Vocab.uuidResource(uuid));
+            var name = Option.of(spec.get("name"))
+                .map(ObjSpec::jsonString);
+
+            var memberOf = maybeUuidList(spec, "memberOf");
+            var subclassOf =  maybeUuidList(spec, "subclassOf");
+            var enumMembers = maybeUuidList(spec, "enum");
+
+            var powersetOf = Option.of(spec.get("powersetOf"))
+                .map(ObjSpec::jsonString)
+                .map(Decoders::parseUUIDOrError);
+
+            return new ObjSpec(uuid, iri, primary, name,
+                memberOf, subclassOf, powersetOf, enumMembers);
+        }
+    }
+
     private BulkOperations (RequestHandler req)
     {
         super(req);
@@ -40,10 +90,31 @@ public class BulkOperations extends RequestHandler.Component
         return new BulkOperations(req);
     }
 
-    public void loadDump (JsonValue dump)
+    private static Iterator<java.util.Map.Entry<String, JsonValue>>
+        jsonEntries (JsonValue obj)
     {
-        if (!db().schemaTracker().validateDump(dump))
+        return Iterator.ofAll(
+            ((JsonObject)obj).entrySet());
+    }
+
+    public void loadDump (JsonValue jval)
+    {
+        if (!db().schemaTracker().validateDump(jval))
             throw new SvcErr.BadInput("Invalid dump");
         log.info("Dump validated");
+
+        var dump = (JsonObject)jval;
+        Option.of(dump.get("objects"))
+            .map(objs -> jsonEntries(objs)
+                .flatMap(e1 -> jsonEntries(e1.getValue())
+                    .map(e2 -> ObjSpec.ofJson(
+                        e1.getKey(), e2.getKey(), e2.getValue())))
+                .toList())
+            .peek(this::loadObjects);
+    }
+
+    private void loadObjects (List<ObjSpec> objs)
+    {
+        log.info("Decoded obj specs: {}", objs);
     }
 }

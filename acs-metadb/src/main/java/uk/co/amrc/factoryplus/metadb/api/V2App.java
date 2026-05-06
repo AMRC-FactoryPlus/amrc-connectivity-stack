666dea73b4d08fead2c8f32ffd30c7d3f2c1b518
Ben Morrow <b.morrow@amrc.co.uk>
2026-05-06 14:14:58 +0100

    Implement find-within-class

--- acs-metadb/src/main/java/uk/co/amrc/factoryplus/metadb/api/V2App.java
+++ acs-metadb/src/main/java/uk/co/amrc/factoryplus/metadb/api/V2App.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.*;
 
+import io.vavr.Lazy;
 import io.vavr.collection.*;
 import io.vavr.control.*;
 
@@ -34,17 +35,19 @@ public class V2App {
     @Inject private RdfStore db;
     @Inject private SecurityContext auth;
 
-    @PathParam("app")       private UUID app;
-
     /* Because we are pulling from dataflow we do not need our own txns.
      * Dataflow will open a txn if it needs to do an initial fetch and
      * doesn't have cached results already. This means we don't want to
-     * use requestCalculate and need to build our own RequestHandler. */
+     * use requestRead and need to build our own RequestHandler. */
+    private Lazy<RequestHandler> req = Lazy.of(() ->
+        new RequestHandler(db, auth)
+            .fetchACL());
+
+    @PathParam("app")       private UUID app;
+
     private void checkACL (UUID perm, UUID targ)
     {
-        new RequestHandler(db, auth)
-            .fetchACL()
-            .checkACL(perm, targ);
+        req.get().checkACL(perm, targ);
     }
 
     private JsonValue uuidSet (Set<UUID> uuids)
@@ -70,13 +73,31 @@ public class V2App {
         return uuidSet(objs);
     }
 
+    private Set<UUID> findAll (JsonValue config)
+    {
+        return appValues()
+            .filterValues(e -> e.value().equals(config))
+            .keySet();
+    }
+
     @POST @Path("find")
     public JsonValue find (JsonValue config)
     {
         checkACL(Vocab.Perm.ReadApp, app);
-        var objs = appValues()
-            .filterValues(e -> e.value().equals(config))
-            .keySet();
-        return uuidSet(objs);
+        return uuidSet(findAll(config));
+    }
+
+    @POST @Path("class/{class}/find")
+    public JsonValue findInClass (@PathParam("class") UUID klass, JsonValue config)
+    {
+        checkACL(Vocab.Perm.ReadApp, app);
+        var rel = Relation.of("member").bind(klass, true, false);
+        checkACL(rel.perm(), klass);
+
+        var objs = findAll(config);
+        var mems = db.dataflow()
+            .relation(rel)
+            .blockingFirst();
+        return uuidSet(objs.intersect(mems));
     }
 }

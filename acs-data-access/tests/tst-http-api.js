e2e10f2362733ea4d2522e3adc3f52b598a0b564
amrc-za <z.abduraimova@sheffield.ac.uk>
2026-06-08 10:07:47 +0100

    data access test scripts

--- acs-data-access/tests/tst-http-api.js
+++ acs-data-access/tests/tst-http-api.js
@@ -1,8 +1,6 @@
 
 
 import {ServiceClient} from "@amrc-factoryplus/service-client";
-import * as rx from "rxjs";
-import readline from "node:readline";
 import process from "node:process";
 
 
@@ -10,20 +8,28 @@ import process from "node:process";
  * This is a test script that uses rx-client DataAccess client interface
  * to subscribe to DataAccess Notify API. 
  */
+
+
+    // .get_metadata_list();
+    // .get_single_metadata(DATASET_UUID)
 async function main(){
     const fplus = new ServiceClient({
         directory_url: process.env.DIRECTORY_URL,
-        username: process.env.HUMAN_USERNAME,
-        password: process.env.HUMAN_PASSWORD,
+        username: process.env.TEST_HUMAN_USERNAME,
+        password: process.env.TEST_HUMAN_PASSWORD,
         verbose: 'ALL'
     });
 
-    const DATASET_UUID = "";
 
-    const res = await fplus.DataAccess
-    // .get_metadata_list();
-    // .get_single_metadata(DATASET_UUID)
-    .download_data(DATASET_UUID)
+    const uuid = "a7594958-4b03-45e4-8ac0-4af8d1e77e3f";
+    const res = await fplus.DataAccess.delete_dataset(uuid);
+
+
+
+
+    // const res = await fplus.DataAccess.download_data(DATASET_UUID)
+
+    // const res = await fplus.DataAccess.get_metadata_list();
 
     console.log("Test Data Access HTTP");
     console.log(res);
@@ -31,4 +37,4 @@ async function main(){
 }
 
 
-main()
+main().catch(console.error);
